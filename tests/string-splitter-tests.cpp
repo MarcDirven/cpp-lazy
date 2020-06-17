525a465e85ce3610108a7324388af30f5cb01127
Marc <marc.dirven@student.hu.nl>
2020-06-17 17:32:39 +0200

    Added MSVC string_view compat

--- tests/string-splitter-tests.cpp
+++ tests/string-splitter-tests.cpp
@@ -18,9 +18,9 @@ TEST_CASE("String splitter changing and creating elements", "[String splitter][B
         }
     }
 
-#if __cplusplus < 201703L
+#if __cplusplus < 201703L || (defined(_MSVC_LANG) && _MSVC_LANG < 201703L)
     SECTION("Should be const std::string&") {
-        CHECK(std::is_same<decltype(*it), const std::string&>::value);
+        CHECK(std::is_same<decltype(*it), std::string&>::value);
     }
 #else
     SECTION("Should be std::string_view") {
