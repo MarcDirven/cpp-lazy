46b867d0b37502ec7ca2fae83a251b5135353afb
Marc <marc.dirven@student.hu.nl>
2020-07-11 12:01:28 +0200

    Added Choose and Random include

--- include/Lz.hpp
+++ include/Lz.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <Lz/Choose.hpp>
 #include <Lz/Enumerate.hpp>
 #include <Lz/Except.hpp>
 #include <Lz/Filter.hpp>
 #include <Lz/Map.hpp>
+#include <Lz/Random.hpp>
 #include <Lz/Range.hpp>
 #include <Lz/Repeat.hpp>
 #include <Lz/StringSplitter.hpp>
 #include <Lz/Take.hpp>
 #include <Lz/TakeEvery.hpp>
 #include <Lz/Zip.hpp>
-
