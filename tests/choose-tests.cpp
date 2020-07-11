6372366c60f77c645731c51624aa66445455af79
Marc <marc.dirven@student.hu.nl>
2020-07-11 23:43:46 +0200

    Added to map function test

--- tests/choose-tests.cpp
+++ tests/choose-tests.cpp
@@ -5,6 +5,7 @@
 #include <cctype>
 #include <iostream>
 #include <list>
+#include <set>
 
 
 TEST_CASE("Choose changing and creating elements", "[Choose][Basic functionality]") {
@@ -65,5 +66,21 @@ TEST_CASE("Choose to container", "[Choose][To container]") {
 
     SECTION("To other container using to<>()") {
         CHECK(chooser.to<std::list>() == std::list<int>{1, 2, 4, 5});
+    }
+
+    SECTION("To map") {
+        int x = 0;
+        std::map<int, int> actual = chooser.toMap([&x](const char s) {
+           return x++;
+        });
+
+        std::map<int, int> expected = {
+            std::make_pair(0, 1),
+            std::make_pair(1, 2),
+            std::make_pair(2, 4),
+            std::make_pair(3, 5)
+        };
+
+        CHECK(expected == actual);
     }
 }
