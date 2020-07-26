ed92dadc7e03293f8ece118c3b13289e37a74d30
Marc <marc.dirven@student.hu.nl>
2020-07-26 23:34:20 +0200

    Added dropwhile

---
+++ include/Lz/DropWhile.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <Lz/detail/DropWhileIterator.hpp>
+#include <Lz/detail/BasicIteratorView.hpp>
+
+
+namespace lz {
+    template<class Iterator, class Function>
+    class DropWhile final : public detail::BasicIteratorView<detail::DropWhileIterator<Iterator, Function>> {
+    public:
+        using iterator = detail::DropWhileIterator<Iterator, Function>;
+        using const_iterator = iterator;
+
+        using value_type = typename iterator::value_type;
+
+    private:
+        iterator _begin{};
+        iterator _end{};
+
+    public:
+        DropWhile(const Iterator begin, const Iterator end, const Function predicate) :
+            _begin(begin, end, predicate),
+            _end(end, end, predicate) {
+        }
+
+        iterator begin() const {
+            return _begin;
+        }
+
+        iterator end() const {
+            return _end;
+        }
+    };
+
+    template<class Iterator, class Function>
+    auto dropwhilerange(const Iterator begin, const Iterator end, const Function predicate) {
+        using ValueType = typename std::iterator_traits<Iterator>::value_type;
+        static_assert(std::is_same<decltype(std::declval<Function>()(std::declval<ValueType>())), bool>::value,
+                      "the function predicate must return a bool");
+        return DropWhile<Iterator, Function>(begin, end, predicate);
+    }
+
+    template<class Iterable, class Function>
+    auto dropwhile(Iterable&& iterable, const Function predicate) {
+        return dropwhilerange(std::begin(iterable), std::end(iterable), predicate);
+    }
+}
---
+++ include/Lz/detail/DropWhileIterator.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+
+namespace lz { namespace detail {
+    template<class Iterator, class Function>
+    class DropWhileIterator {
+    public:
+        using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
+        using value_type = typename std::iterator_traits<Iterator>::value_type;
+        using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+        using reference = typename std::iterator_traits<Iterator>::reference;
+        using pointer = typename std::iterator_traits<Iterator>::pointer;
+
+    private:
+        Iterator _iterator{};
+
+    public:
+        DropWhileIterator(const Iterator begin, const Iterator end, const Function function) :
+            _iterator(begin != end ? std::find_if(begin, end, [function](const value_type& value) {
+                return !function(value);
+            }) : end) {}
+
+        reference operator*() const {
+            return *_iterator;
+        }
+
+        pointer operator->() const {
+            return _iterator.operator->();
+        }
+
+        DropWhileIterator& operator++() {
+            ++_iterator;
+            return *this;
+        }
+
+        DropWhileIterator operator++(int) {
+            auto tmp = *this;
+            ++*this;
+            return tmp;
+        }
+
+        DropWhileIterator& operator--() {
+            --_iterator;
+            return *this;
+        }
+
+        DropWhileIterator operator--(int) {
+            auto tmp(*this);
+            --*this;
+            return tmp;
+        }
+
+        bool operator!=(const DropWhileIterator& other) const {
+            return _iterator != other._iterator;
+        }
+
+        bool operator==(const DropWhileIterator& other) const {
+            return !(*this != other);
+        }
+
+        DropWhileIterator& operator+=(const difference_type offset) {
+            _iterator += offset;
+            return *this;
+        }
+
+        DropWhileIterator& operator-=(const difference_type offset) {
+            _iterator -= offset;
+            return *this;
+        }
+
+        DropWhileIterator operator+(const difference_type offset) const {
+            auto tmp(*this);
+            tmp += offset;
+            return tmp;
+        }
+
+        DropWhileIterator operator-(const difference_type offset) const {
+            auto tmp(*this);
+            tmp -= offset;
+            return tmp;
+        }
+
+        difference_type operator-(const DropWhileIterator& other) const {
+            return _iterator - other._iterator;
+        }
+
+        reference operator[](const difference_type offset) const {
+            return *(*this + offset);
+        }
+
+        bool operator<(const DropWhileIterator& other) const {
+            return _iterator < other._iterator;
+        }
+
+        bool operator>(const DropWhileIterator& other) const {
+            return other < *this;
+        }
+
+        bool operator<=(const DropWhileIterator& other) const {
+            return !(other < *this);
+        }
+
+        bool operator>=(const DropWhileIterator& other) const {
+            return !(*this < other);
+        }
+    };
+}}
