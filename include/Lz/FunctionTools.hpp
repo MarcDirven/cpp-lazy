fc432512729b06038ee98f2ff8bca4abe4974cc6
Marc <marc.dirven@student.hu.nl>
2020-09-10 11:34:14 +0200

    Added median func, unlines, lines and shuffle

--- include/Lz/FunctionTools.hpp
+++ include/Lz/FunctionTools.hpp
@@ -4,12 +4,11 @@
 #include <iterator>
 #include <algorithm>
 
-#include "detail/LzTools.hpp"
-
-
-#if __has_include(<execution>)
-  #define HAS_INCLUDE_EXECUTION
-#endif
+#include "StringSplitter.hpp"
+#include "Join.hpp"
+#include "Map.hpp"
+#include "Random.hpp"
+#include "Enumerate.hpp"
 
 
 namespace lz {
@@ -43,7 +42,7 @@ namespace lz {
             throw std::invalid_argument("the length of the sequence cannot be 0");
         }
 
-        const detail::DifferenceType<Iterator> mid = len / 2;
+        const detail::DifferenceType<Iterator> mid = len >> 1;
         const Iterator midIter = std::next(begin, mid);
         std::nth_element(begin, midIter, end, compare);
 
@@ -59,6 +58,11 @@ namespace lz {
         return median(std::begin(iterable), std::end(iterable), compare);
     }
 
+    template<class Iterable, class Compare>
+    double median(Iterable&& iterable, Compare compare) {
+        return median(std::begin(iterable), std::end(iterable), compare);
+    }
+
     template<class Iterator>
     double median(const Iterator begin, const Iterator end) {
         return median(begin, end, std::less<detail::ValueType<Iterator>>());
@@ -67,5 +71,46 @@ namespace lz {
     template<class Iterable>
     double median(Iterable& iterable) {
         return median(std::begin(iterable), std::end(iterable), std::less<detail::ValueTypeIterable<Iterable>>());
+    }
+
+    template<class Iterable>
+    double median(Iterable&& iterable) {
+        return median(std::begin(iterable), std::end(iterable), std::less<detail::ValueTypeIterable<Iterable>>());
+    }
+
+#ifdef CXX_LT_17
+    template<class SubString = std::string, class String = std::string>
+#else
+    template<class SubString = std::string_view, class String = std::string_view>
+#endif
+    StringSplitter<SubString, String> lines(String&& string) {
+        return split(string, "\n");
+    }
+
+    template<class Strings>
+    auto unlines(Strings&& strings) -> Join<std::decay_t<decltype(std::begin(strings))>> {
+        static_assert(std::is_same<std::string, typename std::decay_t<Strings>::value_type>::value
+#ifndef CXX_LT_17
+            || std::is_same<std::string_view, typename std::decay_t<Strings>::value_type>::value
+#endif
+            , "the type of the container should be std::string or std::string_view");
+        return join(strings, "\n");
+    }
+
+    template<class Iterable>
+    void shuffle(Iterable& iterable) {
+        constexpr size_t min = 0;
+        const auto max = static_cast<size_t>(std::distance(std::begin(iterable), std::end(iterable)));
+        auto indexes = random(min, max, max);
+
+#ifdef CXX_LT_17
+        for (std::pair<size_t, size_t> idxRandomIdx : enumerate(indexes)) {
+            std::swap(iterable[idxRandomIdx.first], iterable[idxRandomIdx.second]);
+        }
+#else
+        for (auto [idx, randomIdx] : enumerated) {
+            std::swap(iterable[idx], iterable[randomIdx]);
+        }
+#endif
     }
 }
