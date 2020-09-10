7d5caefbf86d2c2171d88a64c2ca9488a493bf69
Marc <marc.dirven@student.hu.nl>
2020-09-10 11:34:47 +0200

    Added extra tests for function tools

--- tests/function-tools-tests.cpp
+++ tests/function-tools-tests.cpp
@@ -1,19 +1,47 @@
 #include <Lz/FunctionTools.hpp>
+#include <Lz/Range.hpp>
 #include <catch.hpp>
-#include <utility>
-#include <iostream>
 
 
 TEST_CASE("Function tools") {
-    std::vector<int> array = {1, 2, 3, 4};
+    std::vector<int> ints = {1, 2, 3, 4};
+    std::vector<double> doubles = {1.2, 2.5, 3.3, 4.5};
 
     SECTION("Mean") {
-        double avg = lz::mean(array);
-        CHECK(avg == (1. + 2. + 3. + 4.) / 4.);
+        double avg = lz::mean(ints);
+        CHECK(avg == Approx((1. + 2. + 3. + 4.) / 4.));
     }
 
     SECTION("Median") {
-        double median = lz::median(array);
-        CHECK(median == (2. + 3.) / 2.);
+        double median = lz::median(std::vector<int>(ints));
+        CHECK(median == Approx((2 + 3) / 2.));
+
+        median = lz::median(doubles);
+        CHECK(median == Approx((2.5 + 3.3) / 2.));
+
+        doubles.push_back(3.3);
+        median = lz::median(doubles);
+        CHECK(median == Approx(3.3));
+    }
+
+    SECTION("Unlines") {
+        std::vector<std::string> strings = {"hello", "world", "what's", "up"};
+        auto unlines = lz::unlines(strings).toString();
+        CHECK(unlines == "hello\nworld\nwhat's\nup");
+    }
+
+    SECTION("Lines") {
+        std::string string = "aa\nbb\nbb";
+        auto lines = lz::lines(string).toVector();
+        CHECK(lines == std::vector<std::string>{"aa", "bb", "bb"});
+    }
+
+    SECTION("Shuffle") {
+        constexpr size_t size = 64;
+        std::array<int, size> toShuffle = lz::range<int>(size).toArray<size>();
+        std::array<int, size> checker = toShuffle;
+        lz::shuffle(toShuffle);
+
+        CHECK(toShuffle != checker);
     }
 }
