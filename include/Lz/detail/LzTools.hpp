9fb3d9fbbab27555b619014aa116bf8491ada9c5
Marc <marc.dirven@student.hu.nl>
2020-09-13 00:08:12 +0200

    Added greater or equal than for c++20

--- include/Lz/detail/LzTools.hpp
+++ include/Lz/detail/LzTools.hpp
@@ -7,7 +7,7 @@
 #if __cplusplus < 201703L || (defined(_MSVC_LANG) && _MSVC_LANG < 201703L)
   #define CXX_LT_17
 #endif
-#if __cplusplus > 202002L || (defined(_MSVC_LANG) && _MSVC_LANG > 202002L)
+#if __cplusplus >= 202002L || (defined(_MSVC_LANG) && _MSVC_LANG >= 202002L)
   #define HAS_CXX_20
 #endif
 
