5548d124f123d633aad703ab294ddb7248705db8
MarcDirven <31624073+MarcDirven@users.noreply.github.com>
2020-09-28 17:31:50 +0200

    Update JoinIterator.hpp

--- include/Lz/detail/JoinIterator.hpp
+++ include/Lz/detail/JoinIterator.hpp
@@ -19,7 +19,7 @@
                                                               !std::is_same<T, unsigned char>::value &&
                                                               !std::is_same<T, wchar_t>::value &&
  #ifdef HAS_CXX_20
-                                                              !std::is_same<T, char8_t> &&
+                                                              !std::is_same<T, char8_t>::value &&
  #endif
                                                               !std::is_same<T, char16_t>::value &&
                                                               !std::is_same<T, char32_t>::value> {
