a7e1263556c6b96cfb99a52749eedd8076919e60
Marc <marc.dirven@student.hu.nl>
2020-10-09 18:17:39 +0200

    Added appropriate internal typedef

--- include/Lz/Concatenate.hpp
+++ include/Lz/Concatenate.hpp
@@ -42,12 +42,9 @@ namespace lz {
     template<LZ_CONCEPT_ITERATOR... Iterators>
     Concatenate<Iterators...> concatRange(const std::tuple<Iterators...>& begin, const std::tuple<Iterators...>& end) {
         static_assert(sizeof...(Iterators) >= 2, "amount of iterators/containers cannot be less than or equal to 1");
-        static_assert(internal::IsAllSame<internal::ValueType<Iterators>...>::value,
-                      "value types of iterators do not match");
-        static_assert(internal::IsAllSame<internal::PointerType<Iterators>...>::value,
-                      "pointer types of iterators do not match");
-        static_assert(internal::IsAllSame<internal::RefType<Iterators>...>::value,
-                      "reference types of iterators do not match");
+        static_assert(internal::IsAllSame<internal::ValueType<Iterators>...>::value, "value types of iterators do not match");
+        static_assert(internal::IsAllSame<internal::PointerType<Iterators>...>::value, "pointer types of iterators do not match");
+        static_assert(internal::IsAllSame<internal::RefType<Iterators>...>::value, "reference types of iterators do not match");
 
         return Concatenate<Iterators...>(begin, end);
     }
