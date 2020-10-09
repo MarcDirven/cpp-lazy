eb7f4b78bb76e1e202c3b8cda9a45bce0e468fa4
Marc <marc.dirven@student.hu.nl>
2020-10-09 15:12:04 +0200

    Added extra comments for #endif

--- include/Lz/detail/FilterIterator.hpp
+++ include/Lz/detail/FilterIterator.hpp
@@ -11,9 +11,9 @@
 namespace lz { namespace internal {
 #ifdef LZ_HAS_EXECUTION
     template<class Execution, LZ_CONCEPT_ITERATOR Iterator, class Function>
-#else
+#else // ^^^lz has execution vvv ! lz has execution
     template<LZ_CONCEPT_ITERATOR Iterator, class Function>
-#endif
+#endif // end has execution
     class FilterIterator {
         using IterTraits = std::iterator_traits<Iterator>;
 
@@ -30,32 +30,32 @@ namespace lz { namespace internal {
         Function _predicate;
 #ifdef LZ_HAS_EXECUTION
         Execution _execution{};
-#endif
+#endif // end has execution
 
     public:
 #ifdef LZ_HAS_EXECUTION
         FilterIterator(const Iterator begin, const Iterator end, const Function& function, const Execution execution)
-#else
+#else // ^^^lz has execution vvv ! lz has execution
         FilterIterator(const Iterator begin, const Iterator end, const Function& function)  // NOLINT(modernize-pass-by-value)
-#endif
+#endif // end has execution
     :
             _iterator(begin),
             _end(end),
             _predicate(function)
 #ifdef LZ_HAS_EXECUTION
             , _execution(execution)
-#endif
+#endif // end has execution
             {
 #ifdef LZ_HAS_EXECUTION
-            if constexpr (IsSequencedPolicyV<Execution>) {
+            if constexpr (IsSequencedPolicyV<Execution>) { // prevent verbose errors when iter cat < forward
                 _iterator = std::find_if(_iterator, _end, _predicate);
             }
             else {
                 _iterator = std::find_if(_execution, _iterator, _end, _predicate);
             }
-#else
+#else // ^^^lz has execution vvv ! lz has execution
             _iterator = std::find_if(_iterator, _end, _predicate);
-#endif
+#endif // end has execution
         }
 
         FilterIterator() = default;
@@ -77,9 +77,9 @@ namespace lz { namespace internal {
                 else {
                     _iterator = std::find_if(_execution, ++_iterator, _end, _predicate);
                 }
-#else
+#else // ^^^lz has execution vvv ! lz has execution
                 _iterator = std::find_if(++_iterator, _end, _predicate);
-#endif
+#endif // end has execution
             }
             return *this;
         }
