b1d1c1430d56f5b8e61d9a2b25e4bc6284cdb7ac
Marc <marc.dirven@student.hu.nl>
2020-10-22 13:21:47 +0200

    Removed commented test code, added chain tests

--- tests/lz-chain-tests.cpp
+++ tests/lz-chain-tests.cpp
@@ -2,18 +2,29 @@
 #include <catch2/catch.hpp>
 
 
-
 TEST_CASE("Iterator chaining") {
-    constexpr int size = 1024;
+    constexpr int size = 16;
     std::array<int, size> arr = lz::range(size).toArray<size>();
     std::array<int, size> arr2 = lz::range(size).toArray<size>();
-//
-//    auto mapper = lz::map(arr, [=](int i)  { return i;});
-//    auto mapMany = lz::mapMany([](int, int) { return 1; }, mapper, arr2);
-//    auto filter = lz::filter(mapper, [=](int) { return true; });
-    lz::toIter(arr)
-        .map([](int i){ return i; })
-        /*.foldl(0, [](int acc, int next) { return acc + next; })*/;
+    int result = lz::toIter(arr)
+        .as<int&>()
+        .map([](int& i) -> int& { return i; })
+        .forEach([x = 0](int& i) mutable { i = x++;})
+        .zip(arr2)
+        .map([](std::tuple<int, int> tup) { return std::get<0>(tup); })
+        .concat(arr2)
+        .foldl(0, [](int acc, int next) { return acc + next; });
 
-    fmt::print("{}\n", "h");
+    CHECK(result == 240);
+
+    arr = lz::repeat(1, size).toArray<size>();
+    bool isAllSame = lz::toIter(arr)
+        .affirm(std::string("Sequence is not 1"), [](int i) { return i == 1; })
+        .take(size)
+        .takeWhile([](int i) { return i == 1; })
+        .dropWhile([](int i) { return i != 1; })
+        .enumerate()
+        .all([](std::pair<int, int> i) { return i.second == 1; });
+
+    CHECK(isAllSame);
 }
