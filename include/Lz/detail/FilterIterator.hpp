0813382e9350894954085c5eb1fc4ec8120eaeaf
Marc <marc.dirven@student.hu.nl>
2020-10-26 16:24:13 +0100

    Added more std advance

--- include/Lz/detail/FilterIterator.hpp
+++ include/Lz/detail/FilterIterator.hpp
@@ -27,15 +27,16 @@ namespace lz { namespace internal {
         using reference = typename IterTraits::reference;
 
         void find(const difference_type offset) {
+            std::advance(_iterator, offset);
 #ifdef LZ_HAS_EXECUTION
             if constexpr (IsSequencedPolicyV<Execution>) { // prevent verbose errors when iter cat < forward
-                _iterator = std::find_if(std::next(_iterator, offset), _end, _predicate);
+                _iterator = std::find_if(_iterator, _end, _predicate);
             }
             else {
-                _iterator = std::find_if(_execution, std::next(_iterator, offset), _end, _predicate);
+                _iterator = std::find_if(_execution, _iterator, _end, _predicate);
             }
 #else // ^^^lz has execution vvv ! lz has execution
-            _iterator = std::find_if(std::next(_iterator, offset), _end, _predicate);
+            _iterator = std::find_if(_iterator, _end, _predicate);
 #endif // end has execution
         }
 
