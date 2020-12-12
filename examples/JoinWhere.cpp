fc746776163e32b4d4ae7e3a02cc333cbada2a29
Marc <marc.dirven@student.hu.nl>
2020-12-12 15:07:18 +0100

    Added joinWhere example

---
+++ examples/JoinWhere.cpp
@@ -0,0 +1,38 @@
+#include <Lz/JoinWhere.hpp>
+
+struct Customer {
+    int id;
+};
+
+struct PaymentBill {
+    int customerId;
+    int id;
+};
+
+int main() {
+    std::vector<Customer> customers{
+        Customer{25},
+        Customer{1},
+        Customer{39},
+        Customer{103},
+        Customer{99},
+    };
+    std::vector<PaymentBill> paymentBills{
+        PaymentBill{25, 0},
+        PaymentBill{25, 2},
+        PaymentBill{25, 3},
+        PaymentBill{99, 1},
+        PaymentBill{2523, 52},
+        PaymentBill{2523, 53},
+    };
+
+    auto joined = lz::joinWhere(customers, paymentBills,
+                                [](const Customer& p) { return p.id; },
+                                [](const PaymentBill& c) { return c.customerId; },
+                                [](const Customer& p, const PaymentBill& c) { return std::make_tuple(p, c); });
+
+    for (std::tuple<Customer, PaymentBill> : joined) {
+        fmt::print("{} and {} are the same. The corresponding payment bill id is {}",
+                   std::get<0>(joined).id, std::get<1>(joined).customerId, std::get<1>(0).id);
+    }
+}
