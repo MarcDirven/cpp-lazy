f0f4eafc07f668c830485a4686ee74ffd06917a6
Marc <marc.dirven@student.hu.nl>
2020-12-24 21:43:52 +0100

    Added more examples

---
+++ examples/Chunks.cpp
@@ -0,0 +1,13 @@
+#include "Lz/Chunks.hpp"
+
+int main() {
+	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+	auto chunks = lz::chunks(v, 3);
+
+	for (auto&& chunk : chunked) {
+		fmt::print("This chunk has length {}\n", std::distance(chunk.begin(), chunk.end()));
+		for (const int i : chunk) {
+			fmt::print("{}\n", i);
+		}
+	}
+}
