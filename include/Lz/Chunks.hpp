de5140a9f61e8fb45b3789ad7dcea013a26ff3c7
Marc <marc.dirven@student.hu.nl>
2020-12-24 21:44:49 +0100

    Added chunks iterator

---
+++ include/Lz/Chunks.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#ifndef LZ_CHUNKS_HPP
+#define LZ_CHUNKS_HPP
+
+#include "detail/ChunksIterator.hpp"
+
+namespace lz {
+	template<class Iterator>
+	class Chunks final : public internal::BasicIteratorView<internal::ChunksIterator<Iterator>> {
+	public:
+		using iterator = internal::ChunksIterator<Iterator>;
+		using const_iterator = iterator;
+		using value_type = typename iterator::value_type;
+
+		Chunks(Iterator begin, Iterator end, const std::size_t chunkSize):
+			internal::BasicIteratorView<iterator>(iterator(begin, end, chunkSize), iterator(end, end, chunkSize))
+		{}
+
+		Chunks() = default;
+	};
+
+	/**
+	 * Chops a sequence into chunks of `chunkSize`. The value type of the iterator is another iterator, so a double for loop is necessary
+	 * to iterate over.
+	 * @param begin The beginning of the sequence.
+	 * @param end The ending of the sequence.
+	 * @param chunkSize The size of the chunks to be.
+	 * @return A Chunk iterator view object.
+	 */
+	template<LZ_CONCEPT_ITERATOR Iterator>
+	Chunks<Iterator> chunksRange(Iterator begin, Iterator end, const std::size_t chunkSize) {
+		return Chunks<Iterator>(std::move(begin), std::move(end), chunkSize);
+	}
+
+	/**
+	 * Chops a sequence into chunks of `chunkSize`. The value type of the iterator is another iterator, so a double for loop is necessary
+	 * to iterate over.
+	 * @param iterable The sequence to be chopped into chunks.
+	 * @param chunkSize The size of the chunks to be.
+	 * @return A Chunk iterator view object.
+	 */
+	template<LZ_CONCEPT_ITERABLE Iterable>
+	Chunks<internal::IterTypeFromIterable<Iterable>> chunks(Iterable&& iterable, const std::size_t chunkSize) {
+		return chunksRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)), chunkSize);
+	}
+}
+
+#endif //LZ_CHUNKS_HPP
---
+++ tests/chunks-tests.cpp
@@ -0,0 +1,101 @@
+#include "Lz/Chunks.hpp"
+#include "catch2/catch.hpp"
+
+
+TEST_CASE("Chunks changing and creating elements", "[Chunks][Basic functionality]") {
+	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+	auto chunked = lz::chunks(v, 3);
+
+	SECTION("Should be by ref") {
+		auto it = chunked.begin();
+		*it->begin() = 0;
+		CHECK(v[0] == 0);
+	}
+
+	SECTION("Should be correct length") {
+		CHECK(std::distance(chunked.begin(), chunked.end()) == 3);
+	}
+}
+
+TEST_CASE("Chunks binary operations", "[Chunks][Binary ops]") {
+	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+	auto chunked = lz::chunks(v, 3);
+	auto it = chunked.begin();
+
+	SECTION("Operator++") {
+		CHECK(*it->begin() == 1);
+		++it;
+		CHECK(*it->begin() == 4);
+		++it;
+		CHECK(*it->begin() == 7);
+	}
+
+	SECTION("Operator--") {
+		++it;
+		CHECK(*it->begin() == 4);
+		--it;
+		CHECK(*it->begin() == 1);
+		CHECK(it == chunked.begin());
+
+		CHECK(*--chunked.end()->end() == 8);
+	}
+
+	SECTION("Operator== & operator!=") {
+		CHECK(it == chunked.begin());
+		CHECK(it != chunked.end());
+		it = chunked.end();
+		CHECK(it != chunked.begin());
+		CHECK(it == chunked.end());
+	}
+}
+
+TEST_CASE("Chunks to containers", "[Chunk][To container]") {
+	std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+	auto chunked = lz::chunks(v, 3);
+
+	SECTION("To array") {
+		std::array<std::array<int, 3>, 3> arrays{};
+		std::size_t i = 0;
+		for (auto&& chunk : chunked) {
+			std::copy(chunk.begin(), chunk.end(), arrays[i++].begin());
+		}
+
+		std::array<std::array<int, 3>, 3> expected = {
+			std::array<int, 3>{1, 2, 3},
+			std::array<int, 3>{4, 5, 6},
+			std::array<int, 3>{7, 8}
+		};
+
+		CHECK(arrays == expected);
+	}
+
+	SECTION("To vector") {
+		std::vector<std::vector<int>> vectors{};
+		for (auto&& chunk : chunked) {
+			vectors.emplace_back(chunk.toVector());
+		}
+
+		std::vector<std::vector<int>> expected = {
+			std::vector<int>{1, 2, 3},
+			std::vector<int>{4, 5, 6},
+			std::vector<int>{7, 8}
+		};
+
+		CHECK(vectors == expected);
+	}
+
+	SECTION("To other container using to<>()") {
+		std::list<std::list<int>> lists{};
+		for (auto&& chunk : chunked) {
+			lists.emplace_back(chunk.to<std::list>());
+		}
+
+		std::list<std::list<int>> expected = {
+			std::list<int>{1, 2, 3},
+			std::list<int>{4, 5, 6},
+			std::list<int>{7, 8}
+		};
+
+		CHECK(lists == expected);
+	}
+}
