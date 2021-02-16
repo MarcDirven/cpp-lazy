9b87d2f292b65dc9341c318c0686e14a4e8373f6
Marc <marc.dirven@student.hu.nl>
2021-02-16 12:35:24 +0100

    Added extra examples

--- examples/GroupBy.cpp
+++ examples/GroupBy.cpp
@@ -13,4 +13,32 @@ int main() {
 			fmt::print("{} belongs in group {}\n", pair.first, str);
 		}
 	}
+
+	/* Output:
+	 * String length group: 3
+	 * 3 belongs in group i'm
+	 * String length group: 4
+	 * 4 belongs in group done
+	 * String length group: 5
+	 * 5 belongs in group hello
+	 * 5 belongs in group hellp
+	 * */
+
+	fmt::print("\n");
+
+	auto grouperDesc = lz::groupByDescending(vec, [](const std::string& s) { return s.length(); });
+	for (auto&& pair : grouperDesc) {
+		fmt::print("String length group: {}\n", pair.first);
+		for (const auto& str : pair.second) {
+			fmt::print("{} belongs in group {}\n", pair.first, str);
+		}
+	}
+	/*String length group: 5
+	 * 5 belongs in group hello
+	 * 5 belongs in group hellp
+	 * String length group: 4
+	 * 4 belongs in group done
+	 * String length group: 3
+	 * 3 belongs in group i'm
+	 * */
 }
