6a42603fb8132f7f15c2e792dc4d1e53ecd27036
Marc <marc.dirven@student.hu.nl>
2021-03-31 00:55:32 +0200

    Added a new exclude iterator

---
+++ include/Lz/Exclude.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef LZ_EXCLUDE_HPP
+#define LZ_EXCLUDE_HPP
+
+#include "detail/BasicIteratorView.hpp"
+#include "detail/ExcludeIterator.hpp"
+
+namespace lz {
+	template<class Iterator>
+	class Exclude : public internal::BasicIteratorView<internal::ExcludeIterator<Iterator>> {
+	public:
+		using iterator = internal::ExcludeIterator<Iterator>;
+		using const_iterator = iterator;
+
+		Exclude() = default;
+
+		Exclude(Iterator begin, Iterator end, const internal::DiffType<Iterator> from,
+				const internal::DiffType<Iterator> to):
+			internal::BasicIteratorView<iterator>(iterator(begin, begin, end, from, to),
+										 		  iterator(end, begin, end, from, to))
+		{}
+	};
+
+	template<LZ_CONCEPT_ITERATOR Iterator>
+	Exclude<Iterator> excludeRange(Iterator begin, Iterator end, const internal::DiffType<Iterator> from,
+								   const internal::DiffType<Iterator> to) {
+		LZ_ASSERT(std::distance(begin, end) - to >= 0, "cannot access elements after end or before begin");
+		return Exclude<Iterator>(std::move(begin), std::move(end), from, to);
+	}
+
+	template<LZ_CONCEPT_ITERABLE Iterable, class Iterator = internal::IterTypeFromIterable<Iterable>>
+	Exclude<Iterator> exclude(Iterable&& iterable, const internal::DiffType<Iterator> from,
+							  const internal::DiffType<Iterator> to) {
+		return excludeRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)), from,
+							to);
+	}
+}
+
+
+#endif // LZ_SKIP_HPP
---
+++ tests/exclude-tests.cpp
@@ -0,0 +1,99 @@
+#include "Lz/Exclude.hpp"
+#include <catch2/catch.hpp>
+#include <Lz/FunctionTools.hpp>
+
+
+TEST_CASE("Exclude changing and creating elements", "[Exclude][Basic functionality]") {
+	std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	auto excluded1 = lz::exclude(arr, 3, 5);
+	auto excluded2 = lz::exclude(arr, 0, 2);
+	auto excluded3 = lz::exclude(arr, 8, 10);
+
+	SECTION("Should Exclude out element") {
+		constexpr auto signedSize = static_cast<std::ptrdiff_t>(arr.size() - 2);
+		CHECK(std::distance(excluded1.begin(), excluded1.end()) == signedSize);
+		CHECK(std::distance(excluded2.begin(), excluded2.end()) == signedSize);
+		CHECK(std::distance(excluded3.begin(), excluded3.end()) == signedSize);
+	}
+
+	SECTION("Should be by reference") {
+		*excluded2.begin() = 0;
+		CHECK(arr[2] == 0);
+	}
+}
+
+
+TEST_CASE("Exclude binary operations", "[Exclude][Binary ops]") {
+	std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	auto excluded1 = lz::exclude(arr, 3, 5);
+	auto exBeg1 = excluded1.begin();
+	auto exEnd1 = excluded1.end();
+
+	auto excluded2 = lz::exclude(arr, 0, 2);
+	auto exBeg2 = excluded2.begin();
+	auto exEnd2 = excluded2.end();
+
+	auto excluded3 = lz::exclude(arr, 7, 10);
+	auto exBeg3 = excluded3.begin();
+	auto exEnd3 = excluded3.end();
+
+	SECTION("Operator++") {
+		CHECK(*exBeg1 == 1);
+		++exBeg1, ++exBeg1, ++exBeg1;
+		CHECK(*exBeg1 == 6);
+		CHECK(*++exBeg1 == 7);
+
+		CHECK(*exBeg2 == 3);
+		CHECK(++*exBeg2 == 4);
+
+		CHECK(*exBeg3 == 1);
+		++exBeg3, ++exBeg3,++exBeg3,++exBeg3,++exBeg3,++exBeg3,
+		++exBeg3;
+		CHECK(exBeg3 == excluded3.end());
+	}
+
+	SECTION("Operator== & operator!=") {
+		CHECK(exBeg1 != exEnd1);
+		CHECK(exBeg2 != exEnd2);
+		CHECK(exBeg3 != exEnd3);
+	}
+}
+
+TEST_CASE("Exclude to containers", "[Exclude][To container]") {
+	std::array<int, 10> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+	auto excluded1 = lz::exclude(arr, 3, 5);
+	auto excluded2 = lz::exclude(arr, 0, 2);
+	auto excluded3 = lz::exclude(arr, 7, 10);
+
+	SECTION("To array") {
+		CHECK(excluded1.toArray<8>() == std::array<int, 8>{1, 2, 3, 6, 7, 8, 9, 10});
+		CHECK(excluded2.toArray<8>() == std::array<int, 8>{3, 4, 5, 6, 7, 8, 9, 10});
+		CHECK(excluded3.toArray<7>() == std::array<int, 7>{1, 2, 3, 4, 5, 6, 7});
+	}
+
+	SECTION("To vector") {
+		CHECK(excluded1.toVector() == std::vector<int>{1, 2, 3, 6, 7, 8, 9, 10});
+		CHECK(excluded2.toVector() == std::vector<int>{3, 4, 5, 6, 7, 8, 9, 10});
+		CHECK(excluded3.toVector() == std::vector<int>{1, 2, 3, 4, 5, 6, 7});
+	}
+
+	SECTION("To other container using to<>()") {
+		CHECK(excluded1.to<std::list>() == std::list<int>{1, 2, 3, 6, 7, 8, 9, 10});
+		CHECK(excluded2.to<std::list>() == std::list<int>{3, 4, 5, 6, 7, 8, 9, 10});
+		CHECK(excluded3.to<std::list>() == std::list<int>{1, 2, 3, 4, 5, 6, 7});
+	}
+
+	SECTION("To map") {
+		CHECK(excluded1.toMap([](int i) { return i;}) == std::map<int, int>{{1, 1}, {2, 2}, {3, 3}, {6, 6}, {7, 7}, {8, 8}, {9, 9}, {10, 10}});
+		CHECK(excluded2.toMap([](int i) { return i;}) == std::map<int, int>{{3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8}, {9, 9}, {10, 10}});
+		CHECK(excluded3.toMap([](int i) { return i;}) == std::map<int, int>{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}});
+	}
+
+	SECTION("To unordered map") {
+		CHECK(excluded1.toUnorderedMap([](int i) { return i;}) == std::unordered_map<int, int>{{1, 1}, {2, 2}, {3, 3}, {6, 6}, {7, 7}, {8, 8}, {9, 9}, {10, 10}});
+		CHECK(excluded2.toUnorderedMap([](int i) { return i;}) == std::unordered_map<int, int>{{3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}, {8, 8}, {9, 9}, {10, 10}});
+		CHECK(excluded3.toUnorderedMap([](int i) { return i;}) == std::unordered_map<int, int>{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7}});
+	}
+}
