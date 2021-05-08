2da64e449b8f185695ecdea5a7537713c903ca48
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:19:37 +0200

    Join formatting

--- include/Lz/Join.hpp
+++ include/Lz/Join.hpp
@@ -8,45 +8,12 @@
 
 
 namespace lz {
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	class Join final : public internal::BasicIteratorView<internal::JoinIterator<Iterator>> {
-	public:
-		using iterator = internal::JoinIterator<Iterator>;
-		using const_iterator = iterator;
-		using value_type = typename iterator::value_type;
-
-		/**
-		 * @brief Creates a Join object.
-		 * @details Combines the iterator values followed by the delimiter. It is evaluated in a
-		 * `"[value][delimiter][value][delimiter]..."`-like fashion.
-		 * @param begin The beginning of the sequence.
-		 * @param end The ending of the sequence.
-		 * @param delimiter The delimiter to separate the previous and the next values in the sequence.
-		 * @param difference The difference between `begin` and `end`
-		 */
-		Join(Iterator begin, Iterator end, std::string delimiter, const internal::DiffType<Iterator> difference) :
-			internal::BasicIteratorView<iterator>(iterator(std::move(begin), delimiter, true, difference),
-												  iterator(std::move(end), delimiter, false, difference)) {
-		}
-
-		Join() = default;
-
-		/**
-		 * Because a join iterator already has a delimiter, an additional overload is necessary, where the delimiter is defaulted to blank.
-		 * @param o The output stream.
-		 * @param it The Join object to print.
-		 * @return The output stream.
-		 */
-		friend std::ostream& operator<<(std::ostream& o, const Join<Iterator>& it) {
-			// Join already has a delimiter, default to blank string
-			return o << it.toString();
-		}
-	};
-
-	/**
-	 * @addtogroup ItFns
-	 * @{
-	 */
+template<LZ_CONCEPT_ITERATOR Iterator>
+class Join final : public internal::BasicIteratorView<internal::JoinIterator<Iterator>> {
+public:
+	using iterator = internal::JoinIterator<Iterator>;
+	using const_iterator = iterator;
+	using value_type = typename iterator::value_type;
 
 	/**
 	 * @brief Creates a Join object.
@@ -55,31 +22,65 @@ namespace lz {
 	 * @param begin The beginning of the sequence.
 	 * @param end The ending of the sequence.
 	 * @param delimiter The delimiter to separate the previous and the next values in the sequence.
-	 * @return A Join iterator view object.
+	 * @param difference The difference between `begin` and `end`
 	 */
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	Join<Iterator> joinRange(Iterator begin, Iterator end, std::string delimiter) {
-		return Join<Iterator>(std::move(begin), std::move(end), std::move(delimiter), std::distance(begin, end) * 2 - 1);
+	Join(Iterator begin, Iterator end, std::string delimiter, const internal::DiffType<Iterator> difference) :
+		internal::BasicIteratorView<iterator>(iterator(std::move(begin), delimiter, true, difference),
+											  iterator(std::move(end), delimiter, false, difference)) {
 	}
 
-	/**
-	 * @brief Creates a Join object.
-	 * @details Combines the iterator values followed by the delimiter. It is evaluated in a
-	 * `"[value][delimiter][value][delimiter]..."`-like fashion.
-	 * @param iterable The iterable to join with the delimiter.
-	 * @param delimiter The delimiter to separate the previous and the next values in the sequence.
-	 * @return A Join iterator view object.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable>
-	Join<internal::IterTypeFromIterable<Iterable>> join(Iterable&& iterable, std::string delimiter) {
-		return joinRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
-						 std::move(delimiter));
-	}
+	Join() = default;
 
-	// End of group
 	/**
-	 * @}
+	 * Because a join iterator already has a delimiter, an additional overload is necessary, where the delimiter is defaulted to blank.
+	 * @param o The output stream.
+	 * @param it The Join object to print.
+	 * @return The output stream.
 	 */
+	friend std::ostream& operator<<(std::ostream& o, const Join<Iterator>& it) {
+		// Join already has a delimiter, default to blank string
+		return o << it.toString();
+	}
+};
+
+/**
+ * @addtogroup ItFns
+ * @{
+ */
+
+/**
+ * @brief Creates a Join object.
+ * @details Combines the iterator values followed by the delimiter. It is evaluated in a
+ * `"[value][delimiter][value][delimiter]..."`-like fashion.
+ * @param begin The beginning of the sequence.
+ * @param end The ending of the sequence.
+ * @param delimiter The delimiter to separate the previous and the next values in the sequence.
+ * @return A Join iterator view object.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator>
+Join<Iterator> joinRange(Iterator begin, Iterator end, std::string delimiter) {
+	using lz::distance; using std::distance;
+	return Join<Iterator>(std::move(begin), std::move(end), std::move(delimiter), distance(begin, end) * 2 - 1);
+}
+
+/**
+ * @brief Creates a Join object.
+ * @details Combines the iterator values followed by the delimiter. It is evaluated in a
+ * `"[value][delimiter][value][delimiter]..."`-like fashion.
+ * @param iterable The iterable to join with the delimiter.
+ * @param delimiter The delimiter to separate the previous and the next values in the sequence.
+ * @return A Join iterator view object.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable>
+Join<internal::IterTypeFromIterable<Iterable>> join(Iterable&& iterable, std::string delimiter) {
+	return joinRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
+					 std::move(delimiter));
+}
+
+// End of group
+/**
+ * @}
+ */
 }
 
 #endif
