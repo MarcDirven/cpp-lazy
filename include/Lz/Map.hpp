5fbbe35a6bde686bba73e49a19fc268c19573f90
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:20:13 +0200

    Map formatting

--- include/Lz/Map.hpp
+++ include/Lz/Map.hpp
@@ -8,67 +8,67 @@
 
 
 namespace lz {
-	template<LZ_CONCEPT_ITERATOR Iterator, class Function>
-	class Map final : public internal::BasicIteratorView<internal::MapIterator<Iterator, Function>> {
-	public:
-		using iterator = internal::MapIterator<Iterator, Function>;
-		using const_iterator = iterator;
-		using value_type = typename iterator::value_type;
-
-		/**
-		 * @brief The Map constructor.
-		 * @param begin Beginning of the iterator.
-		 * @param end End of the iterator.
-		 * @param function A function with parameter the value type. It may return anything.
-		 */
-		constexpr Map(Iterator begin, Iterator end, Function function) :
-			internal::BasicIteratorView<iterator>(iterator(std::move(begin), function), iterator(std::move(end), function)) {
-		}
-
-		constexpr Map() = default;
-	};
-
-	// Start of group
-	/**
-	 * @addtogroup ItFns
-	 * @{
-	 */
+template<LZ_CONCEPT_ITERATOR Iterator, class Function>
+class Map final : public internal::BasicIteratorView<internal::MapIterator<Iterator, Function>> {
+public:
+	using iterator = internal::MapIterator<Iterator, Function>;
+	using const_iterator = iterator;
+	using value_type = typename iterator::value_type;
 
 	/**
-	 * @brief Returns a random access map object. If MSVC and the type is an STL iterator, pass a pointer iterator, not
-	 * an actual iterator object.
-	 * @details E.g. `map({std::pair(1, 2), std::pair(3, 2)}, [](std::pair<int, int> pairs) { return pair.first; });`
-	 * will return all pairs first values in the sequence, that is, `1` and `3`.
-	 * @param begin The beginning of the sequence.
-	 * @param end The ending of the sequence.
-	 * @param function A function that takes a value type as parameter. It may return anything.
-	 * @return A Map object from [begin, end) that can be converted to an arbitrary container or can be iterated over
-	 * using `for (auto... lz::map(...))`.
+	 * @brief The Map constructor.
+	 * @param begin Beginning of the iterator.
+	 * @param end End of the iterator.
+	 * @param function A function with parameter the value type. It may return anything.
 	 */
-	template<class Function, LZ_CONCEPT_ITERATOR Iterator>
-	constexpr Map<Iterator, Function> mapRange(Iterator begin, Iterator end, Function function) {
-		return Map<Iterator, Function>(std::move(begin), std::move(end), std::move(function));
+	constexpr Map(Iterator begin, Iterator end, Function function) :
+		internal::BasicIteratorView<iterator>(iterator(std::move(begin), function), iterator(std::move(end), function)) {
 	}
 
-	/**
-	 * @brief Returns a bidirectional map object.
-	 * @details E.g. `map({std::pair(1, 2), std::pair(3, 2)}, [](std::pair<int, int> pairs) { return pair.first; });`
-	 * will return all pairs first values in the sequence, that is, `1` and `3`.
-	 * @param iterable The iterable to do the mapping over.
-	 * @param function A function that takes a value type as parameter. It may return anything.
-	 * @return A Map object that can be converted to an arbitrary container or can be iterated over using
-	 * `for (auto... lz::map(...))`.
-	 */
-	template<class Function, LZ_CONCEPT_ITERABLE Iterable>
-	constexpr Map<internal::IterTypeFromIterable<Iterable>, Function> map(Iterable&& iterable, Function function) {
-		return mapRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
-						std::move(function));
-	}
+	constexpr Map() = default;
+};
 
-	// End of group
-	/**
-	 * @}
-	 */
+// Start of group
+/**
+ * @addtogroup ItFns
+ * @{
+ */
+
+/**
+ * @brief Returns a random access map object. If MSVC and the type is an STL iterator, pass a pointer iterator, not
+ * an actual iterator object.
+ * @details E.g. `map({std::pair(1, 2), std::pair(3, 2)}, [](std::pair<int, int> pairs) { return pair.first; });`
+ * will return all pairs first values in the sequence, that is, `1` and `3`.
+ * @param begin The beginning of the sequence.
+ * @param end The ending of the sequence.
+ * @param function A function that takes a value type as parameter. It may return anything.
+ * @return A Map object from [begin, end) that can be converted to an arbitrary container or can be iterated over
+ * using `for (auto... lz::map(...))`.
+ */
+template<class Function, LZ_CONCEPT_ITERATOR Iterator>
+constexpr Map<Iterator, Function> mapRange(Iterator begin, Iterator end, Function function) {
+	return Map<Iterator, Function>(std::move(begin), std::move(end), std::move(function));
+}
+
+/**
+ * @brief Returns a bidirectional map object.
+ * @details E.g. `map({std::pair(1, 2), std::pair(3, 2)}, [](std::pair<int, int> pairs) { return pair.first; });`
+ * will return all pairs first values in the sequence, that is, `1` and `3`.
+ * @param iterable The iterable to do the mapping over.
+ * @param function A function that takes a value type as parameter. It may return anything.
+ * @return A Map object that can be converted to an arbitrary container or can be iterated over using
+ * `for (auto... lz::map(...))`.
+ */
+template<class Function, LZ_CONCEPT_ITERABLE Iterable>
+constexpr Map<internal::IterTypeFromIterable<Iterable>, Function> map(Iterable&& iterable, Function function) {
+	return mapRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
+					std::move(function));
+}
+
+// End of group
+/**
+ * @}
+ */
 }
 
 #endif
--- include/Lz/detail/MapIterator.hpp
+++ include/Lz/detail/MapIterator.hpp
@@ -9,111 +9,111 @@
 
 
 namespace lz { namespace internal {
-	template<LZ_CONCEPT_ITERATOR Iterator, class Function>
-	class MapIterator {
-		Iterator _iterator{};
-		mutable FunctionContainer<Function> _function{};
+template<LZ_CONCEPT_ITERATOR Iterator, class Function>
+class MapIterator {
+	Iterator _iterator{};
+	mutable FunctionContainer<Function> _function{};
 
-		using IterTraits = std::iterator_traits<Iterator>;
+	using IterTraits = std::iterator_traits<Iterator>;
 
-	public:
-		using reference = decltype(_function(*_iterator));
-		using value_type = Decay<reference>;
-		using iterator_category = typename IterTraits::iterator_category;
-		using difference_type = typename IterTraits::difference_type;
-		using pointer = FakePointerProxy<reference>;
+public:
+	using reference = decltype(_function(*_iterator));
+	using value_type = Decay<reference>;
+	using iterator_category = typename IterTraits::iterator_category;
+	using difference_type = typename IterTraits::difference_type;
+	using pointer = FakePointerProxy<reference>;
 
-		constexpr MapIterator(Iterator iterator, Function function) :
-			_iterator(std::move(iterator)),
-			_function(std::move(function)) {
-		}
+	constexpr MapIterator(Iterator iterator, Function function) :
+		_iterator(std::move(iterator)),
+		_function(std::move(function)) {
+	}
 
-		constexpr MapIterator() = default;
+	constexpr MapIterator() = default;
 
-		constexpr reference operator*() const {
-			return _function(*_iterator);
-		}
+	constexpr reference operator*() const {
+		return _function(*_iterator);
+	}
 
-		constexpr pointer operator->() const {
-			return FakePointerProxy<decltype(**this)>(**this);
-		}
+	constexpr pointer operator->() const {
+		return FakePointerProxy<decltype(**this)>(**this);
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator& operator++() {
-			++_iterator;
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator& operator++() {
+		++_iterator;
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator operator++(int) {
-			MapIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator operator++(int) {
+		MapIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator& operator--() {
-			--_iterator;
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator& operator--() {
+		--_iterator;
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator operator--(int) {
-			MapIterator tmp(*this);
-			--*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator operator--(int) {
+		MapIterator tmp(*this);
+		--*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator& operator+=(const difference_type offset) {
-			_iterator += offset;
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator& operator+=(const difference_type offset) {
+		_iterator += offset;
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator& operator-=(const difference_type offset) {
-			_iterator -= offset;
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator& operator-=(const difference_type offset) {
+		_iterator -= offset;
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator operator+(const difference_type offset) const {
-			MapIterator tmp(*this);
-			tmp += offset;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator operator+(const difference_type offset) const {
+		MapIterator tmp(*this);
+		tmp += offset;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 MapIterator operator-(const difference_type offset) const {
-			MapIterator tmp(*this);
-			tmp -= offset;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 MapIterator operator-(const difference_type offset) const {
+		MapIterator tmp(*this);
+		tmp -= offset;
+		return tmp;
+	}
 
-		constexpr difference_type operator-(const MapIterator& other) const {
-			return _iterator - other._iterator;
-		}
+	constexpr friend difference_type operator-(const MapIterator& a, const MapIterator& b) {
+		return a._iterator - b._iterator;
+	}
 
-		LZ_CONSTEXPR_CXX_14 reference operator[](const difference_type offset) const {
-			return *(*this + offset);
-		}
+	LZ_CONSTEXPR_CXX_14 reference operator[](const difference_type offset) const {
+		return *(*this + offset);
+	}
 
-		constexpr friend bool operator==(const MapIterator& a, const MapIterator& b) {
-			return !(a != b); // NOLINT
-		}
+	constexpr friend bool operator==(const MapIterator& a, const MapIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		constexpr friend bool operator!=(const MapIterator& a, const MapIterator& b) {
-			return a._iterator != b._iterator;
-		}
+	constexpr friend bool operator!=(const MapIterator& a, const MapIterator& b) {
+		return a._iterator != b._iterator;
+	}
 
-		constexpr friend bool operator<(const MapIterator& a, const MapIterator& b) {
-			return a._iterator < b._iterator;
-		}
+	constexpr friend bool operator<(const MapIterator& a, const MapIterator& b) {
+		return a._iterator < b._iterator;
+	}
 
-		constexpr friend bool operator>(const MapIterator& a, const MapIterator& b) {
-			return b < a;
-		}
+	constexpr friend bool operator>(const MapIterator& a, const MapIterator& b) {
+		return b < a;
+	}
 
-		constexpr friend bool operator<=(const MapIterator& a, const MapIterator& b) {
-			return !(b < a); // NOLINT
-		}
+	constexpr friend bool operator<=(const MapIterator& a, const MapIterator& b) {
+		return !(b < a); // NOLINT
+	}
 
-		constexpr friend bool operator>=(const MapIterator& a, const MapIterator& b) {
-			return !(a < b); // NOLINT
-		}
-	};
+	constexpr friend bool operator>=(const MapIterator& a, const MapIterator& b) {
+		return !(a < b); // NOLINT
+	}
+};
 }} // end lz::internal
 
 #endif
