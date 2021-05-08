754707c49690afb3e3816a22659274b970d97e6a
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:16:13 +0200

    Flatten formatting

--- include/Lz/Flatten.hpp
+++ include/Lz/Flatten.hpp
@@ -6,66 +6,66 @@
 
 
 namespace lz {
-	template<class Iterator, int Dims>
-	class Flatten final : public internal::BasicIteratorView<internal::FlattenIterator<Iterator, Dims>> {
-	public:
-		using iterator = internal::FlattenIterator<Iterator, Dims>;
-		using const_iterator = iterator;
-		using value_type = typename internal::FlattenIterator<Iterator, 0>::value_type;
+template<class Iterator, int Dims>
+class Flatten final : public internal::BasicIteratorView<internal::FlattenIterator<Iterator, Dims>> {
+public:
+	using iterator = internal::FlattenIterator<Iterator, Dims>;
+	using const_iterator = iterator;
+	using value_type = typename internal::FlattenIterator<Iterator, 0>::value_type;
 
-	private:
-		using Base = internal::BasicIteratorView<iterator>;
+private:
+	using Base = internal::BasicIteratorView<iterator>;
 
-	public:
-		constexpr Flatten() = default;
+public:
+	constexpr Flatten() = default;
 
-		constexpr Flatten(Iterator begin, Iterator end) :
-			Base(iterator(begin, begin, end), iterator(end, begin, end)) {}
-	};
+	constexpr Flatten(Iterator begin, Iterator end) :
+		Base(iterator(begin, begin, end), iterator(end, begin, end)) {}
+};
 
 
-	/**
-	 * This function returns a view object that flattens an n-dimensional array.
-	 * @param begin The beginning of the iterable.
-	 * @param end The ending of the iterable.
-	 * @return A flatten view object, where its iterator is a forward iterator.
-	 */
-	template<LZ_CONCEPT_ITERATOR Iterator, int Dims = internal::CountDims<std::iterator_traits<Iterator>>::value - 1>
-	constexpr Flatten<Iterator, Dims> flatten(Iterator begin, Iterator end) {
-		return Flatten<Iterator, Dims>(std::move(begin), std::move(end));
-	}
+/**
+ * This function returns a view object that flattens an n-dimensional array.
+ * @param begin The beginning of the iterable.
+ * @param end The ending of the iterable.
+ * @return A flatten view object, where its iterator is a forward iterator.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator, int Dims = internal::CountDims<std::iterator_traits<Iterator>>::value - 1>
+constexpr Flatten<Iterator, Dims> flatten(Iterator begin, Iterator end) {
+	return Flatten<Iterator, Dims>(std::move(begin), std::move(end));
+}
 
-	/**
-	 * This function returns a view object that flattens an n-dimensional array.
-	 * @param iterable The iterable to flatten.
-	 * @return A flatten view object, where its iterator is a forward iterator.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable,
-		class Iterator = internal::IterTypeFromIterable<Iterable>,
-		int Dims = internal::CountDims<std::iterator_traits<Iterator>>::value - 1>
-	constexpr Flatten<Iterator, Dims> flatten(Iterable&& iterable) {
-		return flatten<Iterator, Dims>(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)));
-	}
+/**
+ * This function returns a view object that flattens an n-dimensional array.
+ * @param iterable The iterable to flatten.
+ * @return A flatten view object, where its iterator is a forward iterator.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable,
+	class Iterator = internal::IterTypeFromIterable<Iterable>,
+	int Dims = internal::CountDims<std::iterator_traits<Iterator>>::value - 1>
+constexpr Flatten<Iterator, Dims> flatten(Iterable&& iterable) {
+	return flatten<Iterator, Dims>(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)));
+}
 
-	/**
-	 * Returns the amount of dimensions an iterator has.
-	 * @param An iterator object.
-	 * @return The amount of dimensions.
-	 */
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	constexpr int dimensionsIter(Iterator) {
-		return internal::CountDims<std::iterator_traits<Iterator>>::value;
-	}
+/**
+ * Returns the amount of dimensions an iterator has.
+ * @param An iterator object.
+ * @return The amount of dimensions.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator>
+constexpr int dimensionsIter(Iterator) {
+	return internal::CountDims<std::iterator_traits<Iterator>>::value;
+}
 
-	/**
-	 * Returns the amount of dimensions a sequence has.
-	 * @param An iterable object.
-	 * @return The amount of dimensions.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable>
-	constexpr int dimensions(Iterable&& it) {
-		return dimensionsIter(internal::begin(std::forward<Iterable>(it)));
-	}
+/**
+ * Returns the amount of dimensions a sequence has.
+ * @param An iterable object.
+ * @return The amount of dimensions.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable>
+constexpr int dimensions(Iterable&& it) {
+	return dimensionsIter(internal::begin(std::forward<Iterable>(it)));
+}
 }
 
 #endif // LZ_FLATTEN_HPP
--- include/Lz/detail/FlattenIterator.hpp
+++ include/Lz/detail/FlattenIterator.hpp
@@ -4,330 +4,327 @@
 #define LZ_FLATTEN_ITERATOR_HPP
 
 #include "LzTools.hpp"
-
+#include <fmt/ostream.h>
 
 namespace lz { namespace internal {
-	template<class T, class U = void>
-	struct AliasWrapper {
-		using Type = U;
-	};
+template<class T, class U = void>
+struct AliasWrapper {
+	using Type = U;
+};
 
-	template<class T, class U = void>
-	using AliasWrapperT = typename AliasWrapper<T, U>::Type;
+template<class T, class U = void>
+using AliasWrapperT = typename AliasWrapper<T, U>::Type;
 
-	template<class T, class Enable = void>
-	struct HasValueType : std::false_type {
-	};
+template<class T, class Enable = void>
+struct HasValueType : std::false_type {
+};
 
-	template<class T>
-	struct HasValueType<T, AliasWrapperT<typename T::value_type>> : std::true_type {
-	};
+template<class T>
+struct HasValueType<T, AliasWrapperT<typename T::value_type>> : std::true_type {
+};
 
-	template<class T, class Enable = void>
-	struct HasDifferenceType : std::false_type {
-	};
+template<class T, class Enable = void>
+struct HasDifferenceType : std::false_type {
+};
 
-	template<class T>
-	struct HasDifferenceType<T, AliasWrapperT<typename T::reference>> : std::true_type {
-	};
+template<class T>
+struct HasDifferenceType<T, AliasWrapperT<typename T::reference>> : std::true_type {
+};
 
-	template<class T, class Enable = void>
-	struct HasPointer : std::false_type {
-	};
+template<class T, class Enable = void>
+struct HasPointer : std::false_type {
+};
 
-	template<class T>
-	struct HasPointer<T, AliasWrapperT<typename T::pointer>> : std::true_type {
-	};
+template<class T>
+struct HasPointer<T, AliasWrapperT<typename T::pointer>> : std::true_type {
+};
 
-	template<class T, class Enable = void>
-	struct HasIterCat : std::false_type {
-	};
+template<class T, class Enable = void>
+struct HasIterCat : std::false_type {
+};
 
-	template<class T>
-	struct HasIterCat<T, AliasWrapperT<typename T::iterator_category>> : std::true_type {
-	};
+template<class T>
+struct HasIterCat<T, AliasWrapperT<typename T::iterator_category>> : std::true_type {
+};
 
-	template<class T, class Enable = void>
-	struct HasReference : std::false_type {
-	};
+template<class T, class Enable = void>
+struct HasReference : std::false_type {
+};
 
-	template<class T>
-	struct HasReference<T, AliasWrapperT<typename T::reference>> : std::true_type {
-	};
+template<class T>
+struct HasReference<T, AliasWrapperT<typename T::reference>> : std::true_type {
+};
 
-	template<class T>
-	struct IsIterator {
-		static constexpr bool value = HasValueType<T>::value &&
-									  HasDifferenceType<T>::value &&
-									  HasPointer<T>::value &&
-									  HasReference<T>::value &&
-									  HasIterCat<T>::value;
-	};
+template<class T>
+struct IsIterator {
+	static constexpr bool value = HasValueType<T>::value &&
+								  HasDifferenceType<T>::value &&
+								  HasPointer<T>::value &&
+								  HasReference<T>::value &&
+								  HasIterCat<T>::value;
+};
 
-	template<class T, class = void>
-	struct IterTraitsOrUnderlyingType {
-		using Type = T;
-	};
+template<class T, class = void>
+struct IterTraitsOrUnderlyingType {
+	using Type = T;
+};
 
-	template<class T>
-	struct IterTraitsOrUnderlyingType<T, AliasWrapperT<typename T::iterator>> {
-		using Type = std::iterator_traits<typename T::iterator>;
-	};
+template<class T>
+struct IterTraitsOrUnderlyingType<T, AliasWrapperT<typename T::iterator>> {
+	using Type = std::iterator_traits<typename T::iterator>;
+};
 
-	template<class T, class U = void>
-	using IterTraitsOrUnderlyingTypeT = typename IterTraitsOrUnderlyingType<T, U>::Type;
+template<class T, class U = void>
+using IterTraitsOrUnderlyingTypeT = typename IterTraitsOrUnderlyingType<T, U>::Type;
 
-	template<class T, bool IsCont>
-	struct CountDimsHelper;
+template<class T, bool IsCont>
+struct CountDimsHelper;
 
-	template<class T>
-	struct CountDimsHelper<T, true> {
-		using Inner = IterTraitsOrUnderlyingTypeT<typename T::value_type>;
-		static constexpr int value = 1 + CountDimsHelper<Inner, IsIterator<Inner>::value>::value;
-	};
+template<class T>
+struct CountDimsHelper<T, true> {
+	using Inner = IterTraitsOrUnderlyingTypeT<typename T::value_type>;
+	static constexpr int value = 1 + CountDimsHelper<Inner, IsIterator<Inner>::value>::value;
+};
 
-	template<class T>
-	struct CountDimsHelper<T, false> {
-		static constexpr int value = 0;
-	};
+template<class T>
+struct CountDimsHelper<T, false> {
+	static constexpr int value = 0;
+};
 
-	template<class T>
-	struct CountDims {
-		static constexpr int value = CountDimsHelper<T, IsIterator<T>::value>::value;
-	};
-
-
-	// Improvement of https://stackoverflow.com/a/21076724/8729023
-	template<class Iterator>
-	class FlattenWrapper {
-		Iterator _begin{};
-		Iterator _current{};
-		Iterator _end{};
-
-		using IterTraits = std::iterator_traits<Iterator>;
-	public:
-		using pointer = typename IterTraits::pointer;
-		using reference = typename IterTraits::reference;
-		using value_type = typename IterTraits::value_type;
-		using iterator_category = LowestIterTypeT<std::bidirectional_iterator_tag, typename IterTraits::iterator_category>;
-		using difference_type = typename IterTraits::difference_type;
-
-		constexpr FlattenWrapper() = default;
-
-		constexpr FlattenWrapper(Iterator current, Iterator begin, Iterator end) :
-			_begin(std::move(begin)),
-			_current(std::move(current)),
-			_end(std::move(end)) {}
-
-		constexpr bool hasSome() const {
-			return _current != _end;
-		}
-
-		constexpr bool hasPrev() const {
-			return _current != _begin;
-		}
-
-		constexpr friend bool operator!=(const FlattenWrapper& a, const FlattenWrapper& b) {
-			return a._current != b._current;
-		}
-
-		constexpr friend bool operator==(const FlattenWrapper& a, const FlattenWrapper& b) {
-			return !(a != b); // NOLINT
-		}
-
-		constexpr reference operator*() const {
-			return *_current;
-		}
-
-		constexpr pointer operator->() const {
-			return &*_current;
-		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenWrapper& operator++() {
-			++_current;
-			return *this;
-		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenWrapper operator++(int) {
-			FlattenWrapper tmp(*this);
-			++*this;
-			return tmp;
-		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenWrapper& operator--() {
-			--_current;
-			return *this;
-		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenWrapper operator--(int) {
-			FlattenWrapper tmp(*this);
-			++*this;
-			return tmp;
-		}
-	};
+template<class T>
+struct CountDims {
+	static constexpr int value = CountDimsHelper<T, IsIterator<T>::value>::value;
+};
 
 
-	template<class Iterator, int N>
-	class FlattenIterator {
-		using Inner = FlattenIterator<decltype(std::begin(*std::declval<Iterator>())), N - 1>;
+// Improvement of https://stackoverflow.com/a/21076724/8729023
+template<class Iterator>
+class FlattenWrapper {
+	Iterator _begin{};
+	Iterator _current{};
+	Iterator _end{};
 
-	public:
-		using pointer = typename Inner::pointer;
-		using reference = typename Inner::reference;
-		using value_type = typename Inner::value_type;
-		using iterator_category = LowestIterTypeT<std::bidirectional_iterator_tag, typename Inner::iterator_category>;
-		using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+	using IterTraits = std::iterator_traits<Iterator>;
+public:
+	using pointer = typename IterTraits::pointer;
+	using reference = typename IterTraits::reference;
+	using value_type = typename IterTraits::value_type;
+	using iterator_category = typename std::common_type<std::bidirectional_iterator_tag, typename IterTraits::iterator_category>::type;
+	using difference_type = typename IterTraits::difference_type;
 
-	private:
-		LZ_CONSTEXPR_CXX_14 void advance() {
+	constexpr FlattenWrapper() = default;
+
+	constexpr FlattenWrapper(Iterator current, Iterator begin, Iterator end) noexcept:
+		_begin(std::move(begin)),
+		_current(std::move(current)),
+		_end(std::move(end)) {}
+
+	constexpr bool hasSome() const {
+		return _current != _end;
+	}
+
+	constexpr bool hasPrev() const {
+		return _current != _begin;
+	}
+
+	constexpr friend bool operator!=(const FlattenWrapper& a, const FlattenWrapper& b) {
+		return a._current != b._current;
+	}
+
+	constexpr friend bool operator==(const FlattenWrapper& a, const FlattenWrapper& b) {
+		return !(a != b); // NOLINT
+	}
+
+	constexpr reference operator*() const {
+		return *_current;
+	}
+
+	constexpr pointer operator->() const {
+		return &*_current;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenWrapper& operator++() {
+		++_current;
+		return *this;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenWrapper operator++(int) {
+		FlattenWrapper tmp(*this);
+		++*this;
+		return tmp;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenWrapper& operator--() {
+		--_current;
+		return *this;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenWrapper operator--(int) {
+		FlattenWrapper tmp(*this);
+		++*this;
+		return tmp;
+	}
+};
+
+
+template<class Iterator, int N>
+class FlattenIterator {
+	using Inner = FlattenIterator<decltype(std::begin(*std::declval<Iterator>())), N - 1>;
+
+public:
+	using pointer = typename Inner::pointer;
+	using reference = typename Inner::reference;
+	using value_type = typename Inner::value_type;
+	using iterator_category = typename std::common_type<std::bidirectional_iterator_tag, typename Inner::iterator_category>::type;
+	using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+
+private:
+	LZ_CONSTEXPR_CXX_14 void advance() {
+		if (_innerIter.hasSome()) { return; }
+		for (++_outerIter; _outerIter.hasSome(); ++_outerIter) {
+			_innerIter = Inner(std::begin(*_outerIter), std::begin(*_outerIter), std::end(*_outerIter));
 			if (_innerIter.hasSome()) { return; }
-
-			for (++_outerIter; _outerIter.hasSome(); ++_outerIter) {
-				_innerIter = Inner(std::begin(*_outerIter), std::begin(*_outerIter), std::end(*_outerIter));
-				if (_innerIter.hasSome()) { return; }
-			}
-			_innerIter = Inner();
 		}
+		_innerIter = Inner();
+	}
 
-		FlattenWrapper<Iterator> _outerIter{};
-		Inner _innerIter{};
+	FlattenWrapper<Iterator> _outerIter{};
+	Inner _innerIter{};
 
-	public:
-		constexpr FlattenIterator() = default;
+public:
+	constexpr FlattenIterator() = default;
 
-		LZ_CONSTEXPR_CXX_14 FlattenIterator(Iterator it, Iterator begin, Iterator end) :
-			_outerIter(std::move(it), std::move(begin), std::move(end)) {
-			if (_outerIter.hasSome()) {
-				_innerIter = Inner(std::begin(*_outerIter), std::begin(*_outerIter), std::end(*_outerIter));
-				this->advance();
-			}
-		}
-
-		constexpr bool hasSome() const {
-			return _outerIter.hasSome();
-		}
-
-		constexpr bool hasPrev() const {
-			return _innerIter.hasPrev() || _outerIter.hasPrev();
-		}
-
-		constexpr friend bool operator!=(const FlattenIterator& a, const FlattenIterator& b) {
-			return a._outerIter != b._outerIter || a._innerIter != b._innerIter;
-		}
-
-		constexpr friend bool operator==(const FlattenIterator& a, const FlattenIterator& b) {
-			return !(a != b); // NOLINT
-		}
-
-		constexpr reference operator*() const {
-			return *_innerIter;
-		}
-
-		constexpr pointer operator->() const {
-			return &*_innerIter;
-		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenIterator& operator++() {
-			++_innerIter;
+	LZ_CONSTEXPR_CXX_14 FlattenIterator(Iterator it, Iterator begin, Iterator end) noexcept:
+		_outerIter(std::move(it), std::move(begin), std::move(end)) {
+		if (_outerIter.hasSome()) {
+			_innerIter = Inner(std::begin(*_outerIter), std::begin(*_outerIter), std::end(*_outerIter));
 			this->advance();
+		}
+	}
+
+	constexpr bool hasSome() const {
+		return _outerIter.hasSome();
+	}
+
+	constexpr bool hasPrev() const {
+		return _innerIter.hasPrev() || _outerIter.hasPrev();
+	}
+
+	constexpr friend bool operator!=(const FlattenIterator& a, const FlattenIterator& b) {
+		return a._outerIter != b._outerIter || a._innerIter != b._innerIter;
+	}
+
+	constexpr friend bool operator==(const FlattenIterator& a, const FlattenIterator& b) {
+		return !(a != b); // NOLINT
+	}
+
+	constexpr reference operator*() const {
+		return *_innerIter;
+	}
+
+	constexpr pointer operator->() const {
+		return &*_innerIter;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenIterator& operator++() {
+		++_innerIter;
+		this->advance();
+		return *this;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenIterator operator++(int) {
+		FlattenIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
+
+	LZ_CONSTEXPR_CXX_14 FlattenIterator& operator--() {
+		if (_innerIter.hasPrev()) {
+			--_innerIter;
 			return *this;
 		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenIterator operator++(int) {
-			FlattenIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
-
-		LZ_CONSTEXPR_CXX_14 FlattenIterator& operator--() {
+		while (_outerIter.hasPrev()) {
+			--_outerIter;
+			_innerIter = Inner(std::end(*_outerIter), std::begin(*_outerIter), std::end(*_outerIter));
 			if (_innerIter.hasPrev()) {
 				--_innerIter;
 				return *this;
 			}
-
-			while (_outerIter.hasPrev()) {
-				--_outerIter;
-				_innerIter = Inner(std::end(*_outerIter), std::begin(*_outerIter), std::end(*_outerIter));
-				if (_innerIter.hasPrev()) {
-					--_innerIter;
-					return *this;
-				}
-
-			}
-			return *this;
 		}
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 FlattenIterator operator--(int) {
-			FlattenIterator tmp(*this);
-			--*this;
-			return tmp;
-		}
-	};
+	LZ_CONSTEXPR_CXX_14 FlattenIterator operator--(int) {
+		FlattenIterator tmp(*this);
+		--*this;
+		return tmp;
+	}
+};
 
 
-	template<class Iterator>
-	class FlattenIterator<Iterator, 0> {
-		FlattenWrapper<Iterator> _range;
-		using Traits = std::iterator_traits<Iterator>;
+template<class Iterator>
+class FlattenIterator<Iterator, 0> {
+	FlattenWrapper<Iterator> _range;
+	using Traits = std::iterator_traits<Iterator>;
 
-	public:
-		using pointer = typename Traits::pointer;
-		using reference = typename Traits::reference;
-		using value_type = typename Traits::value_type;
-		using iterator_category = LowestIterTypeT<std::bidirectional_iterator_tag, typename Traits::iterator_category>;
-		using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+public:
+	using pointer = typename Traits::pointer;
+	using reference = typename Traits::reference;
+	using value_type = typename Traits::value_type;
+	using iterator_category = typename std::common_type<std::bidirectional_iterator_tag, typename Traits::iterator_category>::type;
+	using difference_type = typename std::iterator_traits<Iterator>::difference_type;
 
-		constexpr FlattenIterator() = default;
+	constexpr FlattenIterator() = default;
 
-		constexpr FlattenIterator(Iterator it, Iterator begin, Iterator end) :
-			_range(std::move(it), std::move(begin), std::move(end)) {}
+	constexpr FlattenIterator(Iterator it, Iterator begin, Iterator end) noexcept:
+		_range(std::move(it), std::move(begin), std::move(end)) {}
 
-		constexpr bool hasSome() const {
-			return _range.hasSome();
-		}
+	constexpr bool hasSome() const {
+		return _range.hasSome();
+	}
 
-		constexpr bool hasPrev() const {
-			return _range.hasPrev();
-		}
+	constexpr bool hasPrev() const {
+		return _range.hasPrev();
+	}
 
-		constexpr reference operator*() const {
-			return *_range;
-		}
+	constexpr reference operator*() const {
+		return *_range;
+	}
 
-		constexpr pointer operator->() const {
-			return &*_range;
-		}
+	constexpr pointer operator->() const {
+		return &*_range;
+	}
 
-		constexpr friend bool operator!=(const FlattenIterator& a, const FlattenIterator& b) {
-			return a._range != b._range;
-		}
+	constexpr friend bool operator!=(const FlattenIterator& a, const FlattenIterator& b) {
+		return a._range != b._range;
+	}
 
-		constexpr friend bool operator==(const FlattenIterator& a, const FlattenIterator& b) {
-			return !(a != b); // NOLINT
-		}
+	constexpr friend bool operator==(const FlattenIterator& a, const FlattenIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		LZ_CONSTEXPR_CXX_14 FlattenIterator& operator++() {
-			++_range;
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 FlattenIterator& operator++() {
+		++_range;
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 FlattenIterator operator++(int) {
-			FlattenIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 FlattenIterator operator++(int) {
+		FlattenIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 FlattenIterator& operator--() {
-			--_range;
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 FlattenIterator& operator--() {
+		--_range;
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 FlattenIterator operator--(int) {
-			FlattenIterator tmp(*this);
-			--*this;
-			return tmp;
-		}
-	};
+	LZ_CONSTEXPR_CXX_14 FlattenIterator operator--(int) {
+		FlattenIterator tmp(*this);
+		--*this;
+		return tmp;
+	}
+};
 }}
 
 #endif // LZ_FLATTEN_ITERATOR_HPP
