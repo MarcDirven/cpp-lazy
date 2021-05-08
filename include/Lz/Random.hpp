d6ab7539eff52da1dc330ce429836b830d111d3c
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:20:29 +0200

    Random formatting + random access iterator

--- include/Lz/Random.hpp
+++ include/Lz/Random.hpp
@@ -11,134 +11,134 @@
 
 
 namespace lz {
-	namespace internal {
-		inline std::mt19937 createMtEngine() {
-			std::random_device rd;
-			const auto generator = lz::generate([&rd]() {
-				return rd();
-			}, ((std::mt19937::state_size * sizeof(std::mt19937::result_type)) - 1) / sizeof(std::random_device::result_type) + 1);
-			std::seed_seq sd(generator.begin(), generator.end());
-			return std::mt19937(sd);
-		}
+namespace internal {
+inline std::mt19937 createMtEngine() {
+	std::random_device rd;
+	const auto generator = lz::generate([&rd]() {
+		return rd();
+	}, ((std::mt19937::state_size * sizeof(std::mt19937::result_type)) - 1) / sizeof(std::random_device::result_type) + 1);
+	std::seed_seq sd(generator.begin(), generator.end());
+	return std::mt19937(sd);
+}
+}
+
+template<LZ_CONCEPT_ARITHMETIC Arithmetic, class Distribution, class Generator>
+class Random final : public internal::BasicIteratorView<internal::RandomIterator<Arithmetic, Distribution, Generator>> {
+public:
+	using iterator = internal::RandomIterator<Arithmetic, Distribution, Generator>;
+	using const_iterator = iterator;
+	using value_type = typename iterator::value_type;
+
+	/**
+	 * @brief Random view object constructor, from [`min, max`].
+	 * @param min The minimum value of the random number (included).
+	 * @param max The maximum value of the random number (included).
+	 * @param amount The amount of random numbers to generate. If `std::numeric_limits<size_t>::max()` it is
+	 * interpreted as a `while-true` loop.
+	 * @param isWhileTrueLoop Boolean to indicate if it's a while true loop.
+	 */
+	Random(const Distribution& distribution, Generator& generator, const std::ptrdiff_t amount, const bool isWhileTrueLoop) :
+		internal::BasicIteratorView<iterator>(iterator(distribution, generator, 0, isWhileTrueLoop),
+											  iterator(distribution, generator, amount, isWhileTrueLoop)) {
 	}
 
-	template<LZ_CONCEPT_ARITHMETIC Arithmetic, class Distribution, class Generator>
-	class Random final : public internal::BasicIteratorView<internal::RandomIterator<Arithmetic, Distribution, Generator>> {
-	public:
-		using iterator = internal::RandomIterator<Arithmetic, Distribution, Generator>;
-		using const_iterator = iterator;
-		using value_type = typename iterator::value_type;
-
-		/**
-		 * @brief Random view object constructor, from [`min, max`].
-		 * @param min The minimum value of the random number (included).
-		 * @param max The maximum value of the random number (included).
-		 * @param amount The amount of random numbers to generate. If `std::numeric_limits<size_t>::max()` it is
-		 * interpreted as a `while-true` loop.
-		 * @param isWhileTrueLoop Boolean to indicate if it's a while true loop.
-		 */
-		Random(const Distribution& distribution, Generator& generator, const std::ptrdiff_t amount, const bool isWhileTrueLoop) :
-			internal::BasicIteratorView<iterator>(iterator(distribution, generator, 0, isWhileTrueLoop),
-												  iterator(distribution, generator, amount, isWhileTrueLoop)) {
-		}
-
-		Random() = default;
-
-		/**
-		 * Creates a new random number, not taking into account its size. This is for pure convenience. Example:
-		 * ```cpp
-		 * auto rand = lz::random(0, 5);
-		 * for (int i = 0; i < 50_000; i++) {
-		 *     int myRandom = rand.nextRandom();
-		 * }
-		 * ```
-		 * @return A new random `value_type` between [min, max].
-		 */
-		value_type nextRandom() const {
-			return *this->begin();
-		}
-
-		/**
-		 * Gets the minimum random value.
-		 * @return The min value
-		 */
-		value_type minRandom() const {
-			return this->begin().min();
-		}
-
-		/**
-		 * Gets the maximum random value.
-		 * @return The max value
-		 */
-		value_type maxRandom() const {
-			return this->begin().max();
-		}
-	};
+	Random() = default;
 
 	/**
-	 * @addtogroup ItFns
-	 * @{
+	 * Creates a new random number, not taking into account its size. This is for pure convenience. Example:
+	 * ```cpp
+	 * auto rand = lz::random(0, 5);
+	 * for (int i = 0; i < 50_000; i++) {
+	 *     int myRandom = rand.nextRandom();
+	 * }
+	 * ```
+	 * @return A new random `value_type` between [min, max].
 	 */
-
-	/**
-	 * Creates a random number generator with specified generator and distribution.
-	 * @param distribution A number distribution, for e.g. std::uniform_<type>_distribution<type>.
-	 * @param generator A random number generator, for e.g. std::mt19937.
-	 * @param amount The amount of numbers to create.
-	 * @return A random view object that generates a sequence of `Generator::result_type`
-	 */
-	template<class Generator, class Distribution>
-	Random<typename Distribution::result_type, Distribution, Generator>
-	random(const Distribution& distribution, Generator& generator, const std::size_t amount = std::numeric_limits<std::size_t>::max()) {
-		return Random<typename Distribution::result_type, Distribution, Generator>(
-			distribution, generator, static_cast<std::ptrdiff_t>(amount), amount == std::numeric_limits<std::size_t>::max());
+	value_type nextRandom() const {
+		return *this->begin();
 	}
 
 	/**
-	 * @brief Returns an output iterator view object that generates a sequence of random numbers, using a uniform distribution.
-	 * @details This random access iterator view object can be used to generate a sequence of random numbers between
-	 * [`min, max`]. It uses the std::mt19937 random engine and a seed of `std::random_device` as seed.
-	 * @param min The minimum value , included.
-	 * @param max The maximum value, included.
-	 * @tparam Distribution The distribution for generating the random numbers. `std::uniform_int_distribution` by default.
-	 * @tparam Generator The random number generator. `std::mt19937` by default.
-	 * @param amount The amount of numbers to create. If left empty or equal to `std::numeric_limits<std::size_t>::max()`
-	 * it is interpreted as a `while-true` loop.
-	 * @return A random view object that generates a sequence of random numbers
-	*/
-	template<class Integral, internal::EnableIf<std::is_integral<Integral>::value, bool> = true>
-	Random<Integral, std::uniform_int_distribution<Integral>, std::mt19937>
-	random(const Integral min, const Integral max, const std::size_t amount = std::numeric_limits<std::size_t>::max()) {
-		static std::mt19937 gen = internal::createMtEngine();
-		std::uniform_int_distribution<Integral> dist(min, max);
-		return random(dist, gen, amount);
+	 * Gets the minimum random value.
+	 * @return The min value
+	 */
+	value_type minRandom() const {
+		return this->begin().min();
 	}
 
 	/**
-	 * @brief Returns an output view object that generates a sequence of floating point doubles, using a uniform
-	 * distribution.
-	 * @details This random access iterator view object can be used to generate a sequence of random doubles between
-	 * [`min, max`]. It uses the std::mt19937 random engine and a seed of `std::random_device` as seed.
-	 * @tparam Distribution The distribution for generating the random numbers. `std::uniform_real_distribution` by default.
-	 * @tparam Generator The random number generator. `std::mt19937` by default.
-	 * @param min The minimum value, included.
-	 * @param max The maximum value, included.
-	 * @param amount The amount of numbers to create. If left empty or equal to `std::numeric_limits<std::size_t>::max()`
-	 * it is interpreted as a `while-true` loop.
-	 * @return A random view object that generates a sequence of random floating point values.
+	 * Gets the maximum random value.
+	 * @return The max value
 	 */
-	template<class Floating, internal::EnableIf<std::is_floating_point<Floating>::value, bool> = true>
-	Random<Floating, std::uniform_real_distribution<Floating>, std::mt19937>
-	random(const Floating min, const Floating max, const std::size_t amount = std::numeric_limits<std::size_t>::max()) {
-		static std::mt19937 gen = internal::createMtEngine();
-		std::uniform_real_distribution<Floating> dist(min, max);
-		return random(dist, gen, amount);
+	value_type maxRandom() const {
+		return this->begin().max();
 	}
+};
 
-	// End of group
-	/**
-	 * @}
-	 */
+/**
+ * @addtogroup ItFns
+ * @{
+ */
+
+/**
+ * Creates a random number generator with specified generator and distribution.
+ * @param distribution A number distribution, for e.g. std::uniform_<type>_distribution<type>.
+ * @param generator A random number generator, for e.g. std::mt19937.
+ * @param amount The amount of numbers to create.
+ * @return A random view object that generates a sequence of `Generator::result_type`
+ */
+template<class Generator, class Distribution>
+Random<typename Distribution::result_type, Distribution, Generator>
+random(const Distribution& distribution, Generator& generator, const std::size_t amount = std::numeric_limits<std::size_t>::max()) {
+	return Random<typename Distribution::result_type, Distribution, Generator>(
+		distribution, generator, static_cast<std::ptrdiff_t>(amount), amount == std::numeric_limits<std::size_t>::max());
+}
+
+/**
+ * @brief Returns an output iterator view object that generates a sequence of random numbers, using a uniform distribution.
+ * @details This random access iterator view object can be used to generate a sequence of random numbers between
+ * [`min, max`]. It uses the std::mt19937 random engine and a seed of `std::random_device` as seed.
+ * @param min The minimum value , included.
+ * @param max The maximum value, included.
+ * @tparam Distribution The distribution for generating the random numbers. `std::uniform_int_distribution` by default.
+ * @tparam Generator The random number generator. `std::mt19937` by default.
+ * @param amount The amount of numbers to create. If left empty or equal to `std::numeric_limits<std::size_t>::max()`
+ * it is interpreted as a `while-true` loop.
+ * @return A random view object that generates a sequence of random numbers
+*/
+template<class Integral, internal::EnableIf<std::is_integral<Integral>::value, bool> = true>
+Random<Integral, std::uniform_int_distribution<Integral>, std::mt19937>
+random(const Integral min, const Integral max, const std::size_t amount = std::numeric_limits<std::size_t>::max()) {
+	static std::mt19937 gen = internal::createMtEngine();
+	std::uniform_int_distribution<Integral> dist(min, max);
+	return random(dist, gen, amount);
+}
+
+/**
+ * @brief Returns an output view object that generates a sequence of floating point doubles, using a uniform
+ * distribution.
+ * @details This random access iterator view object can be used to generate a sequence of random doubles between
+ * [`min, max`]. It uses the std::mt19937 random engine and a seed of `std::random_device` as seed.
+ * @tparam Distribution The distribution for generating the random numbers. `std::uniform_real_distribution` by default.
+ * @tparam Generator The random number generator. `std::mt19937` by default.
+ * @param min The minimum value, included.
+ * @param max The maximum value, included.
+ * @param amount The amount of numbers to create. If left empty or equal to `std::numeric_limits<std::size_t>::max()`
+ * it is interpreted as a `while-true` loop.
+ * @return A random view object that generates a sequence of random floating point values.
+ */
+template<class Floating, internal::EnableIf<std::is_floating_point<Floating>::value, bool> = true>
+Random<Floating, std::uniform_real_distribution<Floating>, std::mt19937>
+random(const Floating min, const Floating max, const std::size_t amount = std::numeric_limits<std::size_t>::max()) {
+	static std::mt19937 gen = internal::createMtEngine();
+	std::uniform_real_distribution<Floating> dist(min, max);
+	return random(dist, gen, amount);
+}
+
+// End of group
+/**
+ * @}
+ */
 }
 
 #endif
--- include/Lz/detail/RandomIterator.hpp
+++ include/Lz/detail/RandomIterator.hpp
@@ -7,74 +7,137 @@
 
 
 namespace lz { namespace internal {
-	template<LZ_CONCEPT_ARITHMETIC Arithmetic, class Distribution, class Generator>
-	class RandomIterator {
-	public:
-		using iterator_category = std::forward_iterator_tag;
-		using value_type = Arithmetic;
-		using difference_type = std::ptrdiff_t;
-		using pointer = FakePointerProxy<Arithmetic>;
-		using reference = value_type;
-		using result_type = value_type;
+template<LZ_CONCEPT_ARITHMETIC Arithmetic, class Distribution, class Generator>
+class RandomIterator {
+public:
+	using iterator_category = std::random_access_iterator_tag;
+	using value_type = Arithmetic;
+	using difference_type = std::ptrdiff_t;
+	using pointer = FakePointerProxy<Arithmetic>;
+	using reference = value_type;
+	using result_type = value_type;
 
-	private:
-		std::ptrdiff_t _current{};
-		bool _isWhileTrueLoop{};
-		Distribution _distribution{};
-		Generator* _generator{nullptr};
+private:
+	std::ptrdiff_t _current{};
+	bool _isWhileTrueLoop{};
+	Distribution _distribution{};
+	Generator* _generator{nullptr};
 
-	public:
-		explicit RandomIterator(const Distribution& distribution, Generator& generator, const std::ptrdiff_t current,
-								const bool isWhileTrueLoop) :
-			_current(current),
-			_isWhileTrueLoop(isWhileTrueLoop),
-			_distribution(distribution),
-			_generator(&generator) {
+public:
+	RandomIterator(const Distribution& distribution, Generator& generator, const std::ptrdiff_t current,
+				   const bool isWhileTrueLoop) :
+		_current(current),
+		_isWhileTrueLoop(isWhileTrueLoop),
+		_distribution(distribution),
+		_generator(&generator) {
+	}
+
+	RandomIterator() = default;
+
+	value_type operator*() {
+		return _distribution(*_generator);
+	}
+
+	value_type operator()() {
+		return _distribution(*_generator);
+	}
+
+	pointer operator->() {
+		return FakePointerProxy<decltype(**this)>(**this);
+	}
+
+	result_type (min)() const {
+		return _distribution->min();
+	}
+
+	result_type (max)() const {
+		return _distribution->max();
+	}
+
+	RandomIterator& operator--() {
+		if (!_isWhileTrueLoop) {
+			--_current;
 		}
+		return *this;
+	}
 
-		RandomIterator() = default;
+	RandomIterator operator--(int) {
+		RandomIterator tmp(*this);
+		--*this;
+		return tmp;
+	}
 
-		value_type operator*() {
-			return _distribution(*_generator);
+	RandomIterator& operator+=(const difference_type offset) {
+		if (!_isWhileTrueLoop) {
+			_current += offset;
 		}
+		return *this;
+	}
 
-		result_type (min)() const {
-			return _distribution->min();
-		}
+	RandomIterator operator+(const difference_type offset) const {
+		RandomIterator tmp(*this);
+		tmp += offset;
+		return tmp;
+	}
 
-		result_type (max)() const {
-			return _distribution->max();
+	RandomIterator& operator-=(const difference_type offset) {
+		if (!_isWhileTrueLoop) {
+			_current -= offset;
 		}
+		return *this;
+	}
 
-		value_type operator()() {
-			return _distribution(*_generator);
-		}
+	RandomIterator operator-(const difference_type offset) const {
+		RandomIterator tmp(*this);
+		tmp -= offset;
+		return tmp;
+	}
 
-		pointer operator->() {
-			return FakePointerProxy<decltype(**this)>(**this);
-		}
+	difference_type operator-(const RandomIterator& other) const {
+		return _current - other._current;
+	}
 
-		RandomIterator& operator++() {
-			if (!_isWhileTrueLoop) {
-				++_current;
-			}
-			return *this;
-		}
+	value_type operator[](const difference_type offset) const {
+		return *(*this + offset);
+	}
 
-		RandomIterator operator++(int) {
-			RandomIterator tmp(*this);
-			++*this;
-			return tmp;
+	RandomIterator& operator++() {
+		if (!_isWhileTrueLoop) {
+			++_current;
 		}
+		return *this;
+	}
 
-		friend bool operator!=(const RandomIterator& a, const RandomIterator& b) {
-			return a._current != b._current;
-		}
+	RandomIterator operator++(int) {
+		RandomIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		friend bool operator==(const RandomIterator& a, const RandomIterator& b) {
-			return !(a != b); // NOLINT
-		}
-	};
+	friend bool operator!=(const RandomIterator& a, const RandomIterator& b) {
+		return a._current != b._current;
+	}
+
+	friend bool operator==(const RandomIterator& a, const RandomIterator& b) {
+		return !(a != b); // NOLINT
+	}
+
+	friend bool operator<(const RandomIterator& a, const RandomIterator& b) {
+		return a._current < b._current;
+	}
+
+	friend bool operator>(const RandomIterator& a, const RandomIterator& b) {
+		return b < a;
+	}
+
+	friend bool operator<=(const RandomIterator& a, const RandomIterator& b) {
+		return !(b < a); // NOLINT
+	}
+
+	friend bool operator>=(const RandomIterator& a, const RandomIterator& b) {
+		return !(a < b); // NOLINT
+	}
+};
 }}
 
 #endif
