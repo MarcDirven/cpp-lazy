a34e8e7285139d20755c99d1c6a755a522710ce3
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:22:12 +0200

    Zip formatting + std common type

--- include/Lz/Zip.hpp
+++ include/Lz/Zip.hpp
@@ -8,58 +8,59 @@
 
 
 namespace lz {
-	template<LZ_CONCEPT_ITERATOR... Iterators>
-	class Zip final : public internal::BasicIteratorView<internal::ZipIterator<Iterators...>> {
-	public:
-		using iterator = internal::ZipIterator<Iterators...>;
-		using const_iterator = iterator;
+template<LZ_CONCEPT_ITERATOR... Iterators>
+class Zip final : public internal::BasicIteratorView<internal::ZipIterator<Iterators...>> {
+public:
+	using iterator = internal::ZipIterator<Iterators...>;
+	using const_iterator = iterator;
 
-		using value_type = typename iterator::value_type;
-
-		/**
-		 * @brief This object can be used to iterate over multiple containers. It stops at its smallest container.
-		 * Its `begin()` function returns a random access iterator. The operators `<, <=, >, >=` will return true
-		 * if one of the containers returns true with its corresponding `operator<`/`operator<=`/`operator>`/
-		 * `operator>=`.
-		 * @details The tuple that is returned by `operator*` returns a `std::tuple` by value and its elements by
-		 * reference e.g. `std::tuple<Args&...>`. So it is possible to alter the values in the container/iterable),
-		 * unless the iterator is const, making it a const reference.
-		 * to alter the values in the iterator (and therefore also the container/iterable), unless the iterator is const,
-		 * making it a const reference.
-		 * @param begin The beginning of all the containers
-		 * @param end The ending of all the containers
-		 */
-		LZ_CONSTEXPR_CXX_14 explicit Zip(std::tuple<Iterators...> begin, std::tuple<Iterators...> end) :
-			internal::BasicIteratorView<iterator>(iterator(std::move(begin)), iterator(std::move(end))) {
-		}
-
-		constexpr Zip() = default;
-	};
-
-	// Start of group
-	/**
-	 * @addtogroup ItFns
-	 * @{
-	 */
-
-	template<LZ_CONCEPT_ITERATOR... Iterators>
-	LZ_CONSTEXPR_CXX_14 Zip<Iterators...> zipRange(std::tuple<Iterators...> begin, std::tuple<Iterators...> end) {
-		return Zip<Iterators...>(std::move(begin), std::move(end));
-	}
+	using value_type = typename iterator::value_type;
 
 	/**
-	 * @brief This function can be used to iterate over multiple containers. It stops at its smallest container.
+	 * @brief This object can be used to iterate over multiple containers. It stops at its smallest container.
 	 * Its `begin()` function returns a random access iterator. The operators `<, <=, >, >=` will return true
-	 * if one of the containers returns true with its corresponding `operator<`/`operator<=`/`operator>`/`operator>=`.
+	 * if one of the containers returns true with its corresponding `operator<`/`operator<=`/`operator>`/
+	 * `operator>=`.
 	 * @details The tuple that is returned by `operator*` returns a `std::tuple` by value and its elements by
 	 * reference e.g. `std::tuple<Args&...>`. So it is possible to alter the values in the container/iterable),
 	 * unless the iterator is const, making it a const reference.
-	 * @param iterables The iterables to iterate simultaneously over.
-	 * @return A Take object that can be converted to an arbitrary container or can be iterated over using
-	 * `for (auto tuple :  lz::zip(...))`.
+	 * to alter the values in the iterator (and therefore also the container/iterable), unless the iterator is const,
+	 * making it a const reference.
+	 * @param begin The beginning of all the containers
+	 * @param end The ending of all the containers
 	 */
-	template<LZ_CONCEPT_ITERABLE... Iterables>
-	LZ_CONSTEXPR_CXX_14 Zip<internal::IterTypeFromIterable<Iterables>...> zip(Iterables&& ...iterables) {
+	LZ_CONSTEXPR_CXX_14 explicit Zip(std::tuple<Iterators...> begin, std::tuple<Iterators...> end) :
+		internal::BasicIteratorView<iterator>(iterator(std::move(begin)), iterator(std::move(end))) {
+	}
+
+	constexpr Zip() = default;
+};
+
+// Start of group
+/**
+ * @addtogroup ItFns
+ * @{
+ */
+
+template<LZ_CONCEPT_ITERATOR... Iterators>
+LZ_CONSTEXPR_CXX_14 Zip<Iterators...> zipRange(std::tuple<Iterators...> begin, std::tuple<Iterators...> end) {
+	static_assert(sizeof...(Iterators) > 1, "zip requires more than 1 containers/iterables");
+	return Zip<Iterators...>(std::move(begin), std::move(end));
+}
+
+/**
+ * @brief This function can be used to iterate over multiple containers. It stops at its smallest container.
+ * Its `begin()` function returns a random access iterator. The operators `<, <=, >, >=` will return true
+ * if one of the containers returns true with its corresponding `operator<`/`operator<=`/`operator>`/`operator>=`.
+ * @details The tuple that is returned by `operator*` returns a `std::tuple` by value and its elements by
+ * reference e.g. `std::tuple<Args&...>`. So it is possible to alter the values in the container/iterable),
+ * unless the iterator is const, making it a const reference.
+ * @param iterables The iterables to iterate simultaneously over.
+ * @return A Take object that can be converted to an arbitrary container or can be iterated over using
+ * `for (auto tuple :  lz::zip(...))`.
+ */
+template<LZ_CONCEPT_ITERABLE... Iterables>
+LZ_CONSTEXPR_CXX_14 Zip<internal::IterTypeFromIterable<Iterables>...> zip(Iterables&& ...iterables) {
 	return zipRange(std::make_tuple(internal::begin(std::forward<Iterables>(iterables))...),
 					std::make_tuple(internal::end(std::forward<Iterables>(iterables))...));
 }
--- include/Lz/detail/ZipIterator.hpp
+++ include/Lz/detail/ZipIterator.hpp
@@ -8,160 +8,160 @@
 
 
 namespace lz { namespace internal {
-	template<LZ_CONCEPT_ITERATOR... Iterators>
-	class ZipIterator {
-	public:
-		using iterator_category = LowestIterTypeT<IterCat <Iterators>...>;
-		using value_type = std::tuple<ValueType < Iterators>...>;
-		using difference_type = std::ptrdiff_t;
-		using reference = std::tuple<RefType < Iterators>...>;
-		using pointer = std::tuple<PointerType < Iterators>...>;
+template<LZ_CONCEPT_ITERATOR... Iterators>
+class ZipIterator {
+public:
+	using iterator_category = typename std::common_type<IterCat < Iterators>...>::type;
+	using value_type = std::tuple<ValueType < Iterators>...>;
+	using difference_type = typename std::common_type<DiffType < Iterators>...>::type;
+	using reference = std::tuple<RefType < Iterators>...>;
+	using pointer = std::tuple<PointerType < Iterators>...>;
 
-	private:
-		using MakeIndexSequenceForThis = MakeIndexSequence<sizeof...(Iterators)>;
-		std::tuple<Iterators...> _iterators{};
+private:
+	using MakeIndexSequenceForThis = MakeIndexSequence<sizeof...(Iterators)>;
+	std::tuple<Iterators...> _iterators{};
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_14 reference dereference(IndexSequence<I...>) const {
-			return {*std::get<I>(_iterators)...};
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_14 reference dereference(IndexSequence<I...>) const {
+		return {*std::get<I>(_iterators)...};
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_14 void increment(IndexSequence<I...>) {
-			const int expand[] = {(++std::get<I>(_iterators), 0)...};
-			static_cast<void>(expand);
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_14 void increment(IndexSequence<I...>) {
+		const int expand[] = {(++std::get<I>(_iterators), 0)...};
+		static_cast<void>(expand);
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_14 void decrement(IndexSequence<I...>) {
-			const int expand[] = {(--std::get<I>(_iterators), 0)...};
-			static_cast<void>(expand);
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_14 void decrement(IndexSequence<I...>) {
+		const int expand[] = {(--std::get<I>(_iterators), 0)...};
+		static_cast<void>(expand);
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_14 void plusIs(IndexSequence<I...>, const difference_type differenceType) {
-			const int expand[] = {((std::get<I>(_iterators) += differenceType), 0)...};
-			static_cast<void>(expand);
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_14 void plusIs(IndexSequence<I...>, const difference_type differenceType) {
+		const int expand[] = {((std::get<I>(_iterators) += differenceType), 0)...};
+		static_cast<void>(expand);
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_14 void minIs(IndexSequence<I...>, const difference_type differenceType) {
-			plusIs(MakeIndexSequenceForThis(), -differenceType);
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_14 void minIs(IndexSequence<I...>, const difference_type differenceType) {
+		plusIs(MakeIndexSequenceForThis(), -differenceType);
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_17 difference_type iteratorMin(IndexSequence<I...>, const ZipIterator& other) const {
-			const difference_type diff[] =
-				{static_cast<difference_type>(std::get<I>(_iterators) - std::get<I>(other._iterators)) ...};
-			return static_cast<difference_type>(*std::min_element(std::begin(diff), std::end(diff)));
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_17 difference_type iteratorMin(IndexSequence<I...>, const ZipIterator& other) const {
+		const difference_type diff[] =
+			{static_cast<difference_type>(std::get<I>(_iterators) - std::get<I>(other._iterators)) ...};
+		return static_cast<difference_type>(*std::min_element(std::begin(diff), std::end(diff)));
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_20 bool lessThan(IndexSequence<I...>, const ZipIterator& other) const {
-			const bool distances[] = {(std::get<I>(_iterators) < std::get<I>(other._iterators)) ...};
-			const auto* const end = std::end(distances);
-			return std::find(std::begin(distances), end, true) != end;
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_20 bool lessThan(IndexSequence<I...>, const ZipIterator& other) const {
+		const bool distances[] = {(std::get<I>(_iterators) < std::get<I>(other._iterators)) ...};
+		const auto* const end = std::end(distances);
+		return std::find(std::begin(distances), end, true) != end;
+	}
 
-		template<std::size_t... I>
-		LZ_CONSTEXPR_CXX_20 bool notEqual(IndexSequence<I...>, const ZipIterator& other) const {
-			const bool boolValues[] = {(std::get<I>(_iterators) != std::get<I>(other._iterators))...};
-			const auto* const end = std::end(boolValues);
-			// Check if false not in boolValues
-			return std::find(std::begin(boolValues), end, false) == end;
-		}
+	template<std::size_t... I>
+	LZ_CONSTEXPR_CXX_20 bool notEqual(IndexSequence<I...>, const ZipIterator& other) const {
+		const bool boolValues[] = {(std::get<I>(_iterators) != std::get<I>(other._iterators))...};
+		const auto* const end = std::end(boolValues);
+		// Check if false not in boolValues
+		return std::find(std::begin(boolValues), end, false) == end;
+	}
 
-	public:
-		LZ_CONSTEXPR_CXX_14 explicit ZipIterator(std::tuple<Iterators...> iterators) :
-			_iterators(std::move(iterators)) {
-		}
+public:
+	LZ_CONSTEXPR_CXX_14 explicit ZipIterator(std::tuple<Iterators...> iterators) :
+		_iterators(std::move(iterators)) {
+	}
 
-		constexpr ZipIterator() = default;
+	constexpr ZipIterator() = default;
 
-		LZ_CONSTEXPR_CXX_14 reference operator*() const {
-			return dereference(MakeIndexSequenceForThis());
-		}
+	LZ_CONSTEXPR_CXX_14 reference operator*() const {
+		return dereference(MakeIndexSequenceForThis());
+	}
 
-		LZ_CONSTEXPR_CXX_14 pointer operator->() const {
-			return FakePointerProxy<decltype(**this)>(**this);
-		}
+	LZ_CONSTEXPR_CXX_14 pointer operator->() const {
+		return FakePointerProxy<decltype(**this)>(**this);
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator& operator++() {
-			increment(MakeIndexSequenceForThis());
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator& operator++() {
+		increment(MakeIndexSequenceForThis());
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator operator++(int) {
-			ZipIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator operator++(int) {
+		ZipIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator& operator--() {
-			decrement(MakeIndexSequenceForThis());
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator& operator--() {
+		decrement(MakeIndexSequenceForThis());
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator operator--(int) {
-			auto tmp(*this);
-			--*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator operator--(int) {
+		auto tmp(*this);
+		--*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator& operator+=(const difference_type offset) {
-			plusIs(MakeIndexSequenceForThis(), offset);
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator& operator+=(const difference_type offset) {
+		plusIs(MakeIndexSequenceForThis(), offset);
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator operator+(const difference_type offset) const {
-			ZipIterator tmp(*this);
-			tmp += offset;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator operator+(const difference_type offset) const {
+		ZipIterator tmp(*this);
+		tmp += offset;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator& operator-=(const difference_type offset) {
-			minIs(MakeIndexSequenceForThis(), offset);
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator& operator-=(const difference_type offset) {
+		minIs(MakeIndexSequenceForThis(), offset);
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 ZipIterator operator-(const difference_type offset) const {
-			ZipIterator tmp(*this);
-			tmp -= offset;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 ZipIterator operator-(const difference_type offset) const {
+		ZipIterator tmp(*this);
+		tmp -= offset;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_17 difference_type operator-(const ZipIterator& other) const {
-			return iteratorMin(MakeIndexSequenceForThis(), other);
-		}
+	LZ_CONSTEXPR_CXX_17 difference_type operator-(const ZipIterator& other) const {
+		return iteratorMin(MakeIndexSequenceForThis(), other);
+	}
 
-		LZ_CONSTEXPR_CXX_14 reference operator[](const difference_type offset) const {
-			return *(*this + offset);
-		}
+	LZ_CONSTEXPR_CXX_14 reference operator[](const difference_type offset) const {
+		return *(*this + offset);
+	}
 
-		LZ_CONSTEXPR_CXX_20 friend bool operator==(const ZipIterator& a, const ZipIterator& b) {
-			return !(a != b); // NOLINT
-		}
+	LZ_CONSTEXPR_CXX_20 friend bool operator==(const ZipIterator& a, const ZipIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		LZ_CONSTEXPR_CXX_20 friend bool operator!=(const ZipIterator& a, const ZipIterator& b) {
-			return a.notEqual(MakeIndexSequenceForThis(), b);
-		}
+	LZ_CONSTEXPR_CXX_20 friend bool operator!=(const ZipIterator& a, const ZipIterator& b) {
+		return a.notEqual(MakeIndexSequenceForThis(), b);
+	}
 
-		LZ_CONSTEXPR_CXX_20 friend bool operator<(const ZipIterator& a, const ZipIterator& b) {
-			return a.lessThan(MakeIndexSequenceForThis(), b);
-		}
+	LZ_CONSTEXPR_CXX_20 friend bool operator<(const ZipIterator& a, const ZipIterator& b) {
+		return a.lessThan(MakeIndexSequenceForThis(), b);
+	}
 
-		LZ_CONSTEXPR_CXX_20 friend bool operator>(const ZipIterator& a, const ZipIterator& b) {
-			return b < a;
-		}
+	LZ_CONSTEXPR_CXX_20 friend bool operator>(const ZipIterator& a, const ZipIterator& b) {
+		return b < a;
+	}
 
-		LZ_CONSTEXPR_CXX_20 friend bool operator<=(const ZipIterator& a, const ZipIterator& b) {
-			return !(b < a); // NOLINT
-		}
+	LZ_CONSTEXPR_CXX_20 friend bool operator<=(const ZipIterator& a, const ZipIterator& b) {
+		return !(b < a); // NOLINT
+	}
 
-		LZ_CONSTEXPR_CXX_20 friend bool operator>=(const ZipIterator& a, const ZipIterator& b) {
-			return !(a < b); // NOLINT
-		}
-	};
+	LZ_CONSTEXPR_CXX_20 friend bool operator>=(const ZipIterator& a, const ZipIterator& b) {
+		return !(a < b); // NOLINT
+	}
+};
 }}
 
 #endif
