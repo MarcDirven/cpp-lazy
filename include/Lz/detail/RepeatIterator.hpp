31c3787ee73568e5c17766e15e6f6a7150b61136
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:21:40 +0200

    Repeat formatting

--- include/Lz/detail/RepeatIterator.hpp
+++ include/Lz/detail/RepeatIterator.hpp
@@ -9,130 +9,130 @@
 
 
 namespace lz { namespace internal {
-	template<class T>
-	struct RepeatIteratorHelper {
-		mutable T toRepeat{};
-		bool isWhileTrueLoop{};
+template<class T>
+struct RepeatIteratorHelper {
+	mutable T toRepeat{};
+	bool isWhileTrueLoop{};
 
-		constexpr RepeatIteratorHelper(T toRepeat, const std::size_t amount) :
-			toRepeat(std::move(toRepeat)),
-			isWhileTrueLoop(amount == std::numeric_limits<std::size_t>::max()) {}
+	constexpr RepeatIteratorHelper(T toRepeat, const std::size_t amount) :
+		toRepeat(std::move(toRepeat)),
+		isWhileTrueLoop(amount == std::numeric_limits<std::size_t>::max()) {}
 
-		constexpr RepeatIteratorHelper() = default;
-	};
+	constexpr RepeatIteratorHelper() = default;
+};
 
 
-	template<class T>
-	class RepeatIterator {
-		const RepeatIteratorHelper<T>* _iterHelper{nullptr};
-		std::size_t _iterator{};
+template<class T>
+class RepeatIterator {
+	const RepeatIteratorHelper<T>* _iterHelper{nullptr};
+	std::size_t _iterator{};
 
-	public:
-		using iterator_category = std::random_access_iterator_tag;
-		using value_type = T;
-		using difference_type = std::ptrdiff_t;
-		using pointer = T*;
-		using reference = T&;
+public:
+	using iterator_category = std::random_access_iterator_tag;
+	using value_type = T;
+	using difference_type = std::ptrdiff_t;
+	using pointer = T*;
+	using reference = T&;
 
-		constexpr RepeatIterator(const RepeatIteratorHelper<T>* iteratorHelper, const std::size_t start) :
-			_iterHelper(iteratorHelper),
-			_iterator(start) {
+	constexpr RepeatIterator(const RepeatIteratorHelper<T>* iteratorHelper, const std::size_t start) :
+		_iterHelper(iteratorHelper),
+		_iterator(start) {
+	}
+
+	constexpr RepeatIterator() = default;
+
+	constexpr reference operator*() const {
+		return _iterHelper->toRepeat;
+	}
+
+	constexpr pointer operator->() const {
+		return &_iterHelper->toRepeat;
+	}
+
+	LZ_CONSTEXPR_CXX_14 RepeatIterator& operator++() {
+		if (!_iterHelper->isWhileTrueLoop) {
+			++_iterator;
 		}
+		return *this;
+	}
 
-		constexpr RepeatIterator() = default;
+	LZ_CONSTEXPR_CXX_14 RepeatIterator operator++(int) {
+		RepeatIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		constexpr reference operator*() const {
-			return _iterHelper->toRepeat;
+	LZ_CONSTEXPR_CXX_14 RepeatIterator& operator--() {
+		if (!_iterHelper->isWhileTrueLoop) {
+			--_iterator;
 		}
+		return *this;
+	}
 
-		constexpr pointer operator->() const {
-			return &_iterHelper->toRepeat;
-		}
+	LZ_CONSTEXPR_CXX_14 RepeatIterator operator--(int) {
+		RepeatIterator tmp(*this);
+		--*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator& operator++() {
-			if (!_iterHelper->isWhileTrueLoop) {
-				++_iterator;
-			}
-			return *this;
+	LZ_CONSTEXPR_CXX_14 RepeatIterator& operator+=(const difference_type offset) {
+		if (!_iterHelper->isWhileTrueLoop) {
+			_iterator += offset;
 		}
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator operator++(int) {
-			RepeatIterator tmp(*this);
-			++*this;
-			return tmp;
+	LZ_CONSTEXPR_CXX_14 RepeatIterator& operator-=(const difference_type offset) {
+		if (!_iterHelper->isWhileTrueLoop) {
+			_iterator -= offset;
 		}
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator& operator--() {
-			if (!_iterHelper->isWhileTrueLoop) {
-				--_iterator;
-			}
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 RepeatIterator operator+(const difference_type offset) const {
+		RepeatIterator tmp(*this);
+		tmp += offset;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator operator--(int) {
-			RepeatIterator tmp(*this);
-			--*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_14 RepeatIterator operator-(const difference_type offset) const {
+		RepeatIterator tmp(*this);
+		tmp -= offset;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator& operator+=(const difference_type offset) {
-			if (!_iterHelper->isWhileTrueLoop) {
-				_iterator += offset;
-			}
-			return *this;
-		}
+	constexpr difference_type operator-(const RepeatIterator& other) const {
+		return _iterator - other._iterator;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator& operator-=(const difference_type offset) {
-			if (!_iterHelper->isWhileTrueLoop) {
-				_iterator -= offset;
-			}
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_14 reference operator[](const difference_type offset) const {
+		return *(*this + offset);
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator operator+(const difference_type offset) const {
-			RepeatIterator tmp(*this);
-			tmp += offset;
-			return tmp;
-		}
+	constexpr friend bool operator!=(const RepeatIterator& a, const RepeatIterator& b) {
+		return a._iterator != b._iterator;
+	}
 
-		LZ_CONSTEXPR_CXX_14 RepeatIterator operator-(const difference_type offset) const {
-			RepeatIterator tmp(*this);
-			tmp -= offset;
-			return tmp;
-		}
+	constexpr friend bool operator==(const RepeatIterator& a, const RepeatIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		constexpr difference_type operator-(const RepeatIterator& other) const {
-			return _iterator - other._iterator;
-		}
+	constexpr friend bool operator<(const RepeatIterator& a, const RepeatIterator& b) {
+		return a._iterator < b._iterator;
+	}
 
-		LZ_CONSTEXPR_CXX_14 reference operator[](const difference_type offset) const {
-			return *(*this + offset);
-		}
+	constexpr friend bool operator>(const RepeatIterator& a, const RepeatIterator& b) {
+		return b < a;
+	}
 
-		constexpr friend bool operator!=(const RepeatIterator& a, const RepeatIterator& b) {
-			return a._iterator != b._iterator;
-		}
+	constexpr friend bool operator<=(const RepeatIterator& a, const RepeatIterator& b) {
+		return !(b < a); // NOLINT
+	}
 
-		constexpr friend bool operator==(const RepeatIterator& a, const RepeatIterator& b) {
-			return !(a != b); // NOLINT
-		}
-
-		constexpr friend bool operator<(const RepeatIterator& a, const RepeatIterator& b) {
-			return a._iterator < b._iterator;
-		}
-
-		constexpr friend bool operator>(const RepeatIterator& a, const RepeatIterator& b) {
-			return b < a;
-		}
-
-		constexpr friend bool operator<=(const RepeatIterator& a, const RepeatIterator& b) {
-			return !(b < a); // NOLINT
-		}
-
-		constexpr friend bool operator>=(const RepeatIterator& a, const RepeatIterator& b) {
-			return !(a < b); // NOLINT
-		}
-	};
+	constexpr friend bool operator>=(const RepeatIterator& a, const RepeatIterator& b) {
+		return !(a < b); // NOLINT
+	}
+};
 }}
 
 #endif
