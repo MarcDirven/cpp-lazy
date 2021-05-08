7319c4ab1b1e6180736193b635e6f99f64a7d493
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:22:31 +0200

    String split formating

--- include/Lz/detail/SplitIterator.hpp
+++ include/Lz/detail/SplitIterator.hpp
@@ -13,76 +13,76 @@
 
 
 namespace lz { namespace internal {
-	template<class SubString, class String>
-	class SplitIterator {
-		std::size_t _currentPos{}, _last{};
-		const String* _string{nullptr};
-		std::string _delimiter;
+template<class SubString, class String>
+class SplitIterator {
+	std::size_t _currentPos{}, _last{};
+	const String* _string{nullptr};
+	std::string _delimiter;
 
-	public:
-		using iterator_category = std::forward_iterator_tag;
-		using value_type = SubString;
-		using reference = SubString;
-		using difference_type = std::ptrdiff_t;
-		using pointer = FakePointerProxy<reference>;
+public:
+	using iterator_category = std::forward_iterator_tag;
+	using value_type = SubString;
+	using reference = SubString;
+	using difference_type = std::ptrdiff_t;
+	using pointer = FakePointerProxy<reference>;
 
-		SplitIterator(const std::size_t startingPosition, const String& string, std::string delimiter) :
-			_currentPos(startingPosition),
-			_string(&string),
-			_delimiter(std::move(delimiter)) {
-			// Micro optimization, check if object is created from begin(), only then we want to search
-			if (startingPosition == 0) {
-				_last = _string->find(_delimiter, _currentPos);
-			}
+	SplitIterator(const std::size_t startingPosition, const String& string, std::string delimiter) :
+		_currentPos(startingPosition),
+		_string(&string),
+		_delimiter(std::move(delimiter)) {
+		// Micro optimization, check if object is created from begin(), only then we want to search
+		if (startingPosition == 0) {
+			_last = _string->find(_delimiter, _currentPos);
 		}
+	}
 
-		SplitIterator() = default;
+	SplitIterator() = default;
 
-		value_type operator*() const {
-			if (_last != std::string::npos) {
-				return SubString(&(*_string)[_currentPos], _last - _currentPos);
-			}
-			else {
-				return SubString(&(*_string)[_currentPos]);
-			}
+	value_type operator*() const {
+		if (_last != std::string::npos) {
+			return SubString(&(*_string)[_currentPos], _last - _currentPos);
 		}
-
-		pointer operator->() const {
-			return FakePointerProxy<decltype(**this)>(**this);
+		else {
+			return SubString(&(*_string)[_currentPos]);
 		}
+	}
 
-		friend bool operator!=(const SplitIterator& a, const SplitIterator& b) {
-			return a._currentPos != b._currentPos;
-		}
+	pointer operator->() const {
+		return FakePointerProxy<decltype(**this)>(**this);
+	}
 
-		friend bool operator==(const SplitIterator& a, const SplitIterator& b) {
-			return !(a != b); // NOLINT
-		}
+	friend bool operator!=(const SplitIterator& a, const SplitIterator& b) {
+		return a._currentPos != b._currentPos;
+	}
 
-		SplitIterator& operator++() {
-			const std::size_t delimLen = _delimiter.length();
-			const std::size_t stringLen = _string->length();
-			if (_last == std::string::npos) {
-				_currentPos = stringLen;
-			}
-			else if (_last == stringLen - delimLen) {
-				// Check if ends with delimiter
-				_last = std::string::npos;
-				_currentPos = _string->length();
-			}
-			else {
-				_currentPos = _last + delimLen;
-				_last = _string->find(_delimiter, _currentPos);
-			}
-			return *this;
-		}
+	friend bool operator==(const SplitIterator& a, const SplitIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		SplitIterator operator++(int) {
-			SplitIterator tmp(*this);
-			++*this;
-			return tmp;
+	SplitIterator& operator++() {
+		const std::size_t delimLen = _delimiter.length();
+		const std::size_t stringLen = _string->length();
+		if (_last == std::string::npos) {
+			_currentPos = stringLen;
 		}
-	};
+		else if (_last == stringLen - delimLen) {
+			// Check if ends with delimiter
+			_last = std::string::npos;
+			_currentPos = _string->length();
+		}
+		else {
+			_currentPos = _last + delimLen;
+			_last = _string->find(_delimiter, _currentPos);
+		}
+		return *this;
+	}
+
+	SplitIterator operator++(int) {
+		SplitIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
+};
 }}
 
 #endif
