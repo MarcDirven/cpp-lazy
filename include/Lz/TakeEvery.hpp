6cc52196b6ab462acb4b547eeabd11b36f1494e5
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:22:45 +0200

    TakeEvery formatting + next

--- include/Lz/TakeEvery.hpp
+++ include/Lz/TakeEvery.hpp
@@ -8,73 +8,74 @@
 
 
 namespace lz {
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	class TakeEvery final : public internal::BasicIteratorView<internal::TakeEveryIterator<Iterator>> {
-	public:
-		using iterator = internal::TakeEveryIterator<Iterator>;
-		using const_iterator = iterator;
+template<LZ_CONCEPT_ITERATOR Iterator>
+class TakeEvery final : public internal::BasicIteratorView<internal::TakeEveryIterator<Iterator>> {
+public:
+	using iterator = internal::TakeEveryIterator<Iterator>;
+	using const_iterator = iterator;
 
-		using value_type = typename iterator::value_type;
-
-		/**
-		 * @brief TakeEvery constructor.
-		 * @param begin The beginning of the sequence.
-		 * @param end The ending of the sequence.
-		 * @param offset The offset to add each iteration, aka the amount of elements to skip.
-		 * @param distance The distance between `begin` and `end`.
-		 */
-		constexpr TakeEvery(Iterator begin, Iterator end, const std::size_t offset, typename iterator::difference_type distance) :
-			internal::BasicIteratorView<iterator>(iterator(std::move(begin), end, offset, distance), iterator(end, end, offset, distance)) {
-		}
-
-		constexpr TakeEvery() = default;
-	};
-
-	// Start of group
-	/**
-	 * @addtogroup ItFns
-	 * @{
-	 */
+	using value_type = typename iterator::value_type;
 
 	/**
-	 * @brief This iterator can be used to select elements with `offset` amount. If MSVC and the type is
-	 * an STL iterator, pass a pointer iterator, not an actual iterator object.
-	 * @details If one would like to select every 2nd argument one can use this iterator. Example (pseudo code):
-	 * `takeEvery({1, 2, 3}, 2)`. This will select `1` and `3`. If you would like to skip the first element as well
-	 * one can use: `takeEvery({1, 2, 3}, 2, 2)` the second `2` is the start indexOf, making it select only `3`.
+	 * @brief TakeEvery constructor.
 	 * @param begin The beginning of the sequence.
 	 * @param end The ending of the sequence.
-	 * @param offset The index to add every iteration, aka the index to 'select'.
-	 * @param start The start indexOf, optional. Can be used to skip the first element as well.
-	 * @return A TakeEvery object.
+	 * @param offset The offset to add each iteration, aka the amount of elements to skip.
+	 * @param distance The distance between `begin` and `end`.
 	 */
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	constexpr TakeEvery<Iterator> takeEveryRange(Iterator begin, Iterator end, const internal::DiffType<Iterator> offset,
-												 const internal::DiffType<Iterator> start = 0) {
-		return TakeEvery<Iterator>(std::next(begin, start), end, offset, std::distance(begin, end));
+	constexpr TakeEvery(Iterator begin, Iterator end, const std::size_t offset, typename iterator::difference_type distance) :
+		internal::BasicIteratorView<iterator>(iterator(std::move(begin), end, offset, distance), iterator(end, end, offset, distance)) {
 	}
 
-	/**
-	 * @brief This iterator can be used to select elements with `offset` amount.
-	 * @details If one would like to select every 2nd argument one can use this iterator. Example (pseudo code):
-	 * `takeEvery({1, 2, 3}, 2)`. This will select `1` and `3`. If you would like to skip the first element as well
-	 * one can use: `takeEvery({1, 2, 3}, 2, 2)` the second `2` is the start indexOf, making it select only `3`.
-	 * @param iterable An object that can be iterated over.
-	 * @param offset The index to add every iteration, aka the index to 'select'.
-	 * @param start The start indexOf, optional. Can be used to skip the first element as well.
-	 * @return A TakeEvery object.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable, class Iterator = internal::IterTypeFromIterable<Iterable>>
-	constexpr TakeEvery<Iterator> takeEvery(Iterable&& iterable, const internal::DiffType<Iterator> offset,
-											const internal::DiffType<Iterator> start = 0) {
-		return takeEveryRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
-							  offset, start);
-	}
+	constexpr TakeEvery() = default;
+};
 
-	// End of group
-	/**
-	 * @}
-	 */
+// Start of group
+/**
+ * @addtogroup ItFns
+ * @{
+ */
+
+/**
+ * @brief This iterator can be used to select elements with `offset` amount. If MSVC and the type is
+ * an STL iterator, pass a pointer iterator, not an actual iterator object.
+ * @details If one would like to select every 2nd argument one can use this iterator. Example (pseudo code):
+ * `takeEvery({1, 2, 3}, 2)`. This will select `1` and `3`. If you would like to skip the first element as well
+ * one can use: `takeEvery({1, 2, 3}, 2, 2)` the second `2` is the start indexOf, making it select only `3`.
+ * @param begin The beginning of the sequence.
+ * @param end The ending of the sequence.
+ * @param offset The index to add every iteration, aka the index to 'select'.
+ * @param start The start indexOf, optional. Can be used to skip the first element as well.
+ * @return A TakeEvery object.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator>
+constexpr TakeEvery<Iterator> takeEveryRange(Iterator begin, Iterator end, const internal::DiffType<Iterator> offset,
+											 const internal::DiffType<Iterator> start = 0) {
+	using lz::distance; using std::distance; using std::next; using lz::next;
+	return TakeEvery<Iterator>(next(begin, start), end, offset, distance(begin, end));
+}
+
+/**
+ * @brief This iterator can be used to select elements with `offset` amount.
+ * @details If one would like to select every 2nd argument one can use this iterator. Example (pseudo code):
+ * `takeEvery({1, 2, 3}, 2)`. This will select `1` and `3`. If you would like to skip the first element as well
+ * one can use: `takeEvery({1, 2, 3}, 2, 2)` the second `2` is the start indexOf, making it select only `3`.
+ * @param iterable An object that can be iterated over.
+ * @param offset The index to add every iteration, aka the index to 'select'.
+ * @param start The start indexOf, optional. Can be used to skip the first element as well.
+ * @return A TakeEvery object.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable, class Iterator = internal::IterTypeFromIterable<Iterable>>
+constexpr TakeEvery<Iterator> takeEvery(Iterable&& iterable, const internal::DiffType<Iterator> offset,
+										const internal::DiffType<Iterator> start = 0) {
+	return takeEveryRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
+						  offset, start);
+}
+
+// End of group
+/**
+ * @}
+ */
 }
 
 #endif
--- include/Lz/detail/TakeEveryIterator.hpp
+++ include/Lz/detail/TakeEveryIterator.hpp
@@ -6,67 +6,98 @@
 #include "LzTools.hpp"
 
 
-namespace lz { namespace internal {
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	class TakeEveryIterator {
-		Iterator _iterator{};
-		Iterator _end{};
-		std::size_t _offset{};
-		std::size_t _current{};
-		std::size_t _distance{};
+namespace lz {
+namespace internal {
+template<LZ_CONCEPT_ITERATOR Iterator>
+class TakeEveryIterator {
+	Iterator _iterator{};
+	Iterator _end{};
+	std::size_t _offset{};
+	std::size_t _current{};
+	std::size_t _distance{};
 
-		using IterTraits = std::iterator_traits<Iterator>;
+	using IterTraits = std::iterator_traits<Iterator>;
 
-	public:
-		using value_type = typename IterTraits::value_type;
-		using iterator_category = LowestIterType<std::forward_iterator_tag, typename IterTraits::iterator_category>;
-		using difference_type = typename IterTraits::difference_type;
-		using reference = typename IterTraits::reference;
-		using pointer = typename IterTraits::pointer;
+public:
+	using value_type = typename IterTraits::value_type;
+	using iterator_category = typename std::common_type<std::forward_iterator_tag, typename IterTraits::iterator_category>::type;
+	using difference_type = typename IterTraits::difference_type;
+	using reference = typename IterTraits::reference;
+	using pointer = typename IterTraits::pointer;
 
-		constexpr TakeEveryIterator(Iterator iterator, Iterator end, const std::size_t offset, const std::size_t distance) :
-			_iterator(std::move(iterator)),
-			_end(std::move(end)),
-			_offset(offset),
-			_current(iterator == end ? distance : 0),
-			_distance(distance) {
+	constexpr TakeEveryIterator(Iterator iterator, Iterator end, const std::size_t offset, const std::size_t distance) :
+		_iterator(std::move(iterator)),
+		_end(std::move(end)),
+		_offset(offset),
+		_current(iterator == end ? distance : 0),
+		_distance(distance) {
+	}
+
+	constexpr TakeEveryIterator() = default;
+
+	constexpr reference operator*() const {
+		return *_iterator;
+	}
+
+	constexpr pointer operator->() const {
+		return &*_iterator;
+	}
+
+	LZ_CONSTEXPR_CXX_17 TakeEveryIterator& operator++() {
+		using std::next; using lz::next;
+		if (_current + _offset >= _distance) {
+			_iterator = _end;
 		}
-
-		constexpr TakeEveryIterator() = default;
-
-		constexpr reference operator*() const {
-			return *_iterator;
+		else {
+			_iterator = next(std::move(_iterator), static_cast<difference_type>(_offset));
+			_current += _offset;
 		}
+		return *this;
+	}
 
-		constexpr pointer operator->() const {
-			return &*_iterator;
-		}
+	LZ_CONSTEXPR_CXX_17 TakeEveryIterator operator++(int) {
+		TakeEveryIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_17 TakeEveryIterator& operator++() {
-			if (_current + _offset >= _distance) {
-				_iterator = _end;
-			}
-			else {
-				_iterator = std::next(std::move(_iterator), _offset);
-				_current += _offset;
-			}
-			return *this;
-		}
+	constexpr friend bool operator==(const TakeEveryIterator& a, const TakeEveryIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		LZ_CONSTEXPR_CXX_17 TakeEveryIterator operator++(int) {
-			TakeEveryIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
+	constexpr friend bool operator!=(const TakeEveryIterator& a, const TakeEveryIterator& b) {
+		return a._iterator != b._iterator;
+	}
 
-		constexpr friend bool operator==(const TakeEveryIterator& a, const TakeEveryIterator& b) {
-			return !(a != b); // NOLINT
-		}
+	LZ_CONSTEXPR_CXX_17 friend difference_type operator-(const TakeEveryIterator& a, const TakeEveryIterator& b) {
+		using lz::distance; using std::distance;
+		const auto dist = distance(b._iterator, a._iterator) / static_cast<float>(a._offset);
+		return static_cast<difference_type>(std::ceil(dist));
+	}
 
-		constexpr friend bool operator!=(const TakeEveryIterator& a, const TakeEveryIterator& b) {
-			return a._iterator != b._iterator;
+	LZ_CONSTEXPR_CXX_17 TakeEveryIterator operator+(const difference_type offset) const {
+		using lz::distance; using std::distance; using std::next; using lz::next;
+		const auto dist = distance(_iterator, _end);
+		const auto diffOffset = static_cast<difference_type>(_offset) * offset;
+		if (diffOffset >= dist) {
+			return TakeEveryIterator(_end, _end, _offset, 0);
 		}
-	};
-}}
+		return TakeEveryIterator(next(_iterator, diffOffset), _end, _offset, dist);
+	}
+};
+} // internal
+
+template<class Iterator>
+LZ_CONSTEXPR_CXX_17 typename internal::TakeEveryIterator<Iterator>::difference_type
+distance(const internal::TakeEveryIterator<Iterator>& a, const internal::TakeEveryIterator<Iterator>& b) {
+	return b - a;
+}
+
+template<class Iterator>
+LZ_CONSTEXPR_CXX_17 internal::TakeEveryIterator<Iterator>
+next(const internal::TakeEveryIterator<Iterator>& t, const internal::DiffType<internal::TakeEveryIterator<Iterator>> value) {
+	return t + value;
+}
+} // lz
 
 #endif
