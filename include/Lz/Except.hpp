2dde3e1b88dc5249f2456bd1945935d02bd01152
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:15:28 +0200

    Except formatting

--- include/Lz/Except.hpp
+++ include/Lz/Except.hpp
@@ -11,139 +11,139 @@ namespace lz {
 #ifdef LZ_HAS_EXECUTION
 
 
-	template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare, class Execution>
-	class Except final : public internal::BasicIteratorView<internal::ExceptIterator<Iterator, IteratorToExcept, Compare, Execution>> {
+template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare, class Execution>
+class Except final : public internal::BasicIteratorView<internal::ExceptIterator<Iterator, IteratorToExcept, Compare, Execution>> {
 #else
 
 
-		template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare>
-		class Except final : public internal::BasicIteratorView<internal::ExceptIterator<Iterator, IteratorToExcept, Compare>> {
+	template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare>
+	class Except final : public internal::BasicIteratorView<internal::ExceptIterator<Iterator, IteratorToExcept, Compare>> {
 #endif
-	public:
+public:
 #ifdef LZ_HAS_EXECUTION
-		using iterator = internal::ExceptIterator<Iterator, IteratorToExcept, Compare, Execution>;
+	using iterator = internal::ExceptIterator<Iterator, IteratorToExcept, Compare, Execution>;
 #else
-		using iterator = internal::ExceptIterator<Iterator, IteratorToExcept, Compare>;
+	using iterator = internal::ExceptIterator<Iterator, IteratorToExcept, Compare>;
 #endif
-		using const_iterator = iterator;
-		using value_type = typename iterator::value_type;
+	using const_iterator = iterator;
+	using value_type = typename iterator::value_type;
 
-		/**
-		 * Except constructor. Excepts all elements between [begin, end) contained by [toExceptBegin, toExceptEnd)
-		 * @param begin The beginning of the iterator to skip.
-		 * @param end The ending of the iterator to skip.
-		 * @param toExceptBegin The beginning of the actual elements to except.
-		 * @param toExceptEnd The ending of the actual elements to except.
-		 */
+	/**
+	 * Except constructor. Excepts all elements between [begin, end) contained by [toExceptBegin, toExceptEnd)
+	 * @param begin The beginning of the iterator to skip.
+	 * @param end The ending of the iterator to skip.
+	 * @param toExceptBegin The beginning of the actual elements to except.
+	 * @param toExceptEnd The ending of the actual elements to except.
+	 */
 #ifdef LZ_HAS_EXECUTION
 
-		LZ_CONSTEXPR_CXX_20 Except(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd,
-								   Compare compare, Execution execPolicy) :
-			internal::BasicIteratorView<iterator>(iterator(std::move(begin), end, toExceptBegin, toExceptEnd, compare, execPolicy),
-												  iterator(end, end, toExceptBegin, toExceptEnd, compare, execPolicy)) {}
+	LZ_CONSTEXPR_CXX_20 Except(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd,
+							   Compare compare, Execution execPolicy) :
+		internal::BasicIteratorView<iterator>(iterator(std::move(begin), end, toExceptBegin, toExceptEnd, compare, execPolicy),
+											  iterator(end, end, toExceptBegin, toExceptEnd, compare, execPolicy)) {}
 
 #else // ^^^ has execution vvv ! has execution
 
-		Except(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare) :
-			internal::BasicIteratorView<iterator>(iterator(std::move(begin), end, std::move(toExceptBegin), toExceptEnd, compare),
-												  iterator(end, end, toExceptEnd, toExceptEnd, compare)) {}
+	Except(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare) :
+		internal::BasicIteratorView<iterator>(iterator(std::move(begin), end, std::move(toExceptBegin), toExceptEnd, compare),
+											  iterator(end, end, toExceptEnd, toExceptEnd, compare)) {}
 
 #endif // LZ_HAS_EXECUTION
 
-		constexpr Except() = default;
-	};
+	constexpr Except() = default;
+};
 
-	/**
-	 * @addtogroup ItFns
-	 * @{
-	 */
+/**
+ * @addtogroup ItFns
+ * @{
+ */
 
   #ifdef LZ_HAS_EXECUTION
 
-	/**
-	 * @brief Skips elements in [begin, end) that is contained by [toExceptBegin, toExceptEnd). [toExceptBegin, toExceptEnd) must be sorted
-	 * manually before creating this view.
-	 * @attention [toExceptBegin, toExceptEnd) must be sorted  manually before creating this view.
-	 * @param execPolicy The std::execution::* policy.
-	 * @param begin The beginning of the sequence to skip elements in.
-	 * @param end The ending of the sequence to skip elements in.
-	 * @param toExceptBegin The beginning of the sequence that may not be contained in [begin, end).
-	 * @param toExceptEnd The ending of the sequence that may not be contained in [begin, end).
-	 * @param compare Comparer for binary search (operator < is default) in IteratorToExcept.
-	 * @return An Except view object.
-	 */
-	template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept,
-		class Compare = std::less<>, class Execution = std::execution::sequenced_policy>
-	LZ_CONSTEXPR_CXX_20 Except<Iterator, IteratorToExcept, Compare, Execution>
-	exceptRange(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare = {},
-				Execution execPolicy = std::execution::seq) {
-		return Except<Iterator, IteratorToExcept, Compare, Execution>(std::move(begin), std::move(end), std::move(toExceptBegin),
-																	  std::move(toExceptEnd), std::move(compare), execPolicy);
-	}
+/**
+ * @brief Skips elements in [begin, end) that is contained by [toExceptBegin, toExceptEnd). [toExceptBegin, toExceptEnd) must be sorted
+ * manually before creating this view.
+ * @attention [toExceptBegin, toExceptEnd) must be sorted  manually before creating this view.
+ * @param execPolicy The std::execution::* policy.
+ * @param begin The beginning of the sequence to skip elements in.
+ * @param end The ending of the sequence to skip elements in.
+ * @param toExceptBegin The beginning of the sequence that may not be contained in [begin, end).
+ * @param toExceptEnd The ending of the sequence that may not be contained in [begin, end).
+ * @param compare Comparer for binary search (operator < is default) in IteratorToExcept.
+ * @return An Except view object.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept,
+	class Compare = std::less<>, class Execution = std::execution::sequenced_policy>
+LZ_CONSTEXPR_CXX_20 Except<Iterator, IteratorToExcept, Compare, Execution>
+exceptRange(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare = {},
+			Execution execPolicy = std::execution::seq) {
+	return Except<Iterator, IteratorToExcept, Compare, Execution>(std::move(begin), std::move(end), std::move(toExceptBegin),
+																  std::move(toExceptEnd), std::move(compare), execPolicy);
+}
 
-	/**
-	 * @brief Skips elements iterable that is contained by toExcept. ToExcept must be sorted manually before creating this view.
-	 * @attention ToExcept must be sorted manually before creating this view.
-	 * @param execPolicy The std::execution::* policy.
-	 * @param iterable Sequence to iterate over.
-	 * @param toExcept Sequence that contains items that must be skipped in `iterable`.
-	 * @param compare Comparer for binary search (operator < is default) in IterableToExcept
-	 * @return An Except view object.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable, LZ_CONCEPT_ITERABLE IterableToExcept, class Compare = std::less<>,
-		class Execution = std::execution::sequenced_policy>
-	LZ_CONSTEXPR_CXX_20
-	Except<internal::IterTypeFromIterable<Iterable>, internal::IterTypeFromIterable<IterableToExcept>, Compare, Execution>
-	except(Iterable&& iterable, IterableToExcept&& toExcept, Compare compare = {}, Execution execPolicy = std::execution::seq) {
-		return exceptRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
-						   internal::begin(std::forward<IterableToExcept>(toExcept)),
-						   internal::end(std::forward<IterableToExcept>(toExcept)), std::move(compare), execPolicy);
-	}
+/**
+ * @brief Skips elements iterable that is contained by toExcept. ToExcept must be sorted manually before creating this view.
+ * @attention ToExcept must be sorted manually before creating this view.
+ * @param execPolicy The std::execution::* policy.
+ * @param iterable Sequence to iterate over.
+ * @param toExcept Sequence that contains items that must be skipped in `iterable`.
+ * @param compare Comparer for binary search (operator < is default) in IterableToExcept
+ * @return An Except view object.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable, LZ_CONCEPT_ITERABLE IterableToExcept, class Compare = std::less<>,
+	class Execution = std::execution::sequenced_policy>
+LZ_CONSTEXPR_CXX_20
+Except<internal::IterTypeFromIterable<Iterable>, internal::IterTypeFromIterable<IterableToExcept>, Compare, Execution>
+except(Iterable&& iterable, IterableToExcept&& toExcept, Compare compare = {}, Execution execPolicy = std::execution::seq) {
+	return exceptRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
+					   internal::begin(std::forward<IterableToExcept>(toExcept)),
+					   internal::end(std::forward<IterableToExcept>(toExcept)), std::move(compare), execPolicy);
+}
 
   #else // ^^^ has execution vvv ! has execution
 
-	/**
-	 * @brief Skips elements in [begin, end) that is contained by [toExceptBegin, toExceptEnd). [toExceptBegin, toExceptEnd) must be sorted
-	 * manually before creating this view.
-	 * @attention [toExceptBegin, toExceptEnd) must be sorted  manually before creating this view.
-	 * @param begin The beginning of the sequence to skip elements in.
-	 * @param end The ending of the sequence to skip elements in.
-	 * @param toExceptBegin The beginning of the sequence that may not be contained in [begin, end).
-	 * @param toExceptEnd The ending of the sequence that may not be contained in [begin, end).
-	 * @param compare Comparer for binary search (operator < is default) in IteratorToExcept.
-	 * @return An Except view object.
-	 */
-	template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept,
-		class Compare = std::less<internal::ValueType<Iterator>>>
-	Except<Iterator, IteratorToExcept, Compare>
-	exceptRange(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare = {}) {
-		return Except<Iterator, IteratorToExcept, Compare>(std::move(begin), std::move(end), std::move(toExceptBegin),
-														   std::move(toExceptEnd), std::move(compare));
-	}
+/**
+ * @brief Skips elements in [begin, end) that is contained by [toExceptBegin, toExceptEnd). [toExceptBegin, toExceptEnd) must be sorted
+ * manually before creating this view.
+ * @attention [toExceptBegin, toExceptEnd) must be sorted  manually before creating this view.
+ * @param begin The beginning of the sequence to skip elements in.
+ * @param end The ending of the sequence to skip elements in.
+ * @param toExceptBegin The beginning of the sequence that may not be contained in [begin, end).
+ * @param toExceptEnd The ending of the sequence that may not be contained in [begin, end).
+ * @param compare Comparer for binary search (operator < is default) in IteratorToExcept.
+ * @return An Except view object.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept,
+	class Compare = std::less<internal::ValueType<Iterator>>>
+Except<Iterator, IteratorToExcept, Compare>
+exceptRange(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare = {}) {
+	return Except<Iterator, IteratorToExcept, Compare>(std::move(begin), std::move(end), std::move(toExceptBegin),
+													   std::move(toExceptEnd), std::move(compare));
+}
 
-	/**
-	 * @brief Skips elements iterable that is contained by toExcept. ToExcept must be sorted manually before creating this view.
-	 * @attention ToExcept must be sorted manually before creating this view.
-	 * @param iterable Sequence to iterate over.
-	 * @param toExcept Sequence that contains items that must be skipped in `iterable`.
-	 * @param compare Comparer for binary search (operator < is default) in IterableToExcept
-	 * @return An Except view object.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable, LZ_CONCEPT_ITERABLE IterableToExcept,
-		class Compare = std::less<internal::ValueTypeIterable<Iterable>>>
-	Except<internal::IterTypeFromIterable<Iterable>, internal::IterTypeFromIterable<IterableToExcept>, Compare>
-	except(Iterable&& iterable, IterableToExcept&& toExcept, Compare compare = {}) {
-		return exceptRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
-						   internal::begin(std::forward<IterableToExcept>(toExcept)),
-						   internal::end(std::forward<IterableToExcept>(toExcept)), std::move(compare));
-	}
+/**
+ * @brief Skips elements iterable that is contained by toExcept. ToExcept must be sorted manually before creating this view.
+ * @attention ToExcept must be sorted manually before creating this view.
+ * @param iterable Sequence to iterate over.
+ * @param toExcept Sequence that contains items that must be skipped in `iterable`.
+ * @param compare Comparer for binary search (operator < is default) in IterableToExcept
+ * @return An Except view object.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable, LZ_CONCEPT_ITERABLE IterableToExcept,
+	class Compare = std::less<internal::ValueTypeIterable<Iterable>>>
+Except<internal::IterTypeFromIterable<Iterable>, internal::IterTypeFromIterable<IterableToExcept>, Compare>
+except(Iterable&& iterable, IterableToExcept&& toExcept, Compare compare = {}) {
+	return exceptRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)),
+					   internal::begin(std::forward<IterableToExcept>(toExcept)),
+					   internal::end(std::forward<IterableToExcept>(toExcept)), std::move(compare));
+}
 
   #endif // LZ_HAS_EXECUTION
 
-	// End of group
-	/**
-	 * @}
-	 */
+// End of group
+/**
+ * @}
+ */
 } // end namespace lz
 
 #endif // end LZ_EXCEPT_HPP
--- include/Lz/detail/ExceptIterator.hpp
+++ include/Lz/detail/ExceptIterator.hpp
@@ -13,104 +13,104 @@ namespace lz { namespace internal {
 #ifdef LZ_HAS_EXECUTION
 
 
-	template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare, class Execution>
+template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare, class Execution>
 #else // ^^^ has execution vvv ! has execution
-	template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare>
+template<LZ_CONCEPT_ITERATOR Iterator, LZ_CONCEPT_ITERATOR IteratorToExcept, class Compare>
 #endif // LZ_HAS_EXECUTION
-	class ExceptIterator {
-		using IterTraits = std::iterator_traits<Iterator>;
+class ExceptIterator {
+	using IterTraits = std::iterator_traits<Iterator>;
 
-	public:
-		using iterator_category = std::forward_iterator_tag;
-		using value_type = typename IterTraits::value_type;
-		using difference_type = typename IterTraits::difference_type;
-		using pointer = typename IterTraits::pointer;
-		using reference = typename IterTraits::reference;
+public:
+	using iterator_category = std::forward_iterator_tag;
+	using value_type = typename IterTraits::value_type;
+	using difference_type = typename IterTraits::difference_type;
+	using pointer = typename IterTraits::pointer;
+	using reference = typename IterTraits::reference;
 
-	private:
-		Iterator _iterator{};
-		Iterator _end{};
-		IteratorToExcept _toExceptBegin{};
-		IteratorToExcept _toExceptEnd{};
-		mutable FunctionContainer<Compare> _compare{};
+private:
+	Iterator _iterator{};
+	Iterator _end{};
+	IteratorToExcept _toExceptBegin{};
+	IteratorToExcept _toExceptEnd{};
+	mutable FunctionContainer<Compare> _compare{};
 
 #ifdef LZ_HAS_EXECUTION
-		Execution _execution{};
+	Execution _execution{};
 #endif // LZ_HAS_EXECUTION
 
-		LZ_CONSTEXPR_CXX_20 void find() {
-#ifdef LZ_HAS_EXECUTION
-			if constexpr (internal::checkForwardAndPolicies<Execution, Iterator>()) {
-				_iterator = std::find_if(std::move(_iterator), _end, [this](const value_type& value) {
-					return !std::binary_search(_toExceptBegin, _toExceptEnd, value, _compare);
-				});
-			}
-			else { // NOLINT
-				_iterator = std::find_if(_execution, std::move(_iterator), _end, [this](const value_type& value) {
-					return !std::binary_search(_toExceptBegin, _toExceptEnd, value, _compare);
-				});
-			}
-#else // ^^^ has execution vvv ! has execution
+	LZ_CONSTEXPR_CXX_20 void find() {
+  #ifdef LZ_HAS_EXECUTION
+		if constexpr (internal::checkForwardAndPolicies<Execution, Iterator>()) {
 			_iterator = std::find_if(std::move(_iterator), _end, [this](const value_type& value) {
 				return !std::binary_search(_toExceptBegin, _toExceptEnd, value, _compare);
 			});
-#endif // LZ_HAS_EXECUTION
 		}
+		else { // NOLINT
+			_iterator = std::find_if(_execution, std::move(_iterator), _end, [this](const value_type& value) {
+				return !std::binary_search(_toExceptBegin, _toExceptEnd, value, _compare);
+			});
+		}
+  #else // ^^^ has execution vvv ! has execution
+		_iterator = std::find_if(std::move(_iterator), _end, [this](const value_type& value) {
+			return !std::binary_search(_toExceptBegin, _toExceptEnd, value, _compare);
+		});
+  #endif // LZ_HAS_EXECUTION
+	}
 
-	public:
-		constexpr ExceptIterator() = default;
+public:
+	constexpr ExceptIterator() = default;
 
 #ifdef LZ_HAS_EXECUTION
 
-		LZ_CONSTEXPR_CXX_20 ExceptIterator(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd,
-										   Compare compare, Execution execution) :
+	LZ_CONSTEXPR_CXX_20 ExceptIterator(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd,
+									   Compare compare, Execution execution) :
 #else // ^^^ has execution vvv ! has execution
-			ExceptIterator(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare) :
+		ExceptIterator(Iterator begin, Iterator end, IteratorToExcept toExceptBegin, IteratorToExcept toExceptEnd, Compare compare) :
 #endif // LZ_HAS_EXECUTION
-			_iterator(std::move(begin)),
-			_end(std::move(end)),
-			_toExceptBegin(std::move(toExceptBegin)),
-			_toExceptEnd(std::move(toExceptEnd)),
-			_compare(std::move(compare))
+		_iterator(std::move(begin)),
+		_end(std::move(end)),
+		_toExceptBegin(std::move(toExceptBegin)),
+		_toExceptEnd(std::move(toExceptEnd)),
+		_compare(std::move(compare))
 #ifdef LZ_HAS_EXECUTION
-			,
-			_execution(execution)
+		,
+		_execution(execution)
 #endif // LZ_HAS_EXECUTION
-		{
-			if (_toExceptBegin == _toExceptEnd) {
-				return;
-			}
-			find();
+	{
+		if (_toExceptBegin == _toExceptEnd) {
+			return;
 		}
+		find();
+	}
 
-		constexpr reference operator*() const {
-			return *_iterator;
-		}
+	constexpr reference operator*() const {
+		return *_iterator;
+	}
 
-		constexpr pointer operator->() const {
-			return &*_iterator;
-		}
+	constexpr pointer operator->() const {
+		return &*_iterator;
+	}
 
-		LZ_CONSTEXPR_CXX_20 ExceptIterator& operator++() {
-			++_iterator;
-			find();
-			return *this;
-		}
+	LZ_CONSTEXPR_CXX_20 ExceptIterator& operator++() {
+		++_iterator;
+		find();
+		return *this;
+	}
 
-		LZ_CONSTEXPR_CXX_20 ExceptIterator operator++(int) {
-			ExceptIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
+	LZ_CONSTEXPR_CXX_20 ExceptIterator operator++(int) {
+		ExceptIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		constexpr friend bool operator!=(const ExceptIterator& a, const ExceptIterator& b) {
-			return a._iterator != b._iterator;
-		}
+	constexpr friend bool operator!=(const ExceptIterator& a, const ExceptIterator& b) {
+		return a._iterator != b._iterator;
+	}
 
-		constexpr friend bool operator==(const ExceptIterator& a, const ExceptIterator& b) {
-			return !(a != b); // NOLINT
-		}
-	};
+	constexpr friend bool operator==(const ExceptIterator& a, const ExceptIterator& b) {
+		return !(a != b); // NOLINT
+	}
+};
 }} // end lz::internal
 
 #endif
