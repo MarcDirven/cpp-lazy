4c4a973071af7023562c27ead48d4109cb286042
Marc <marc.dirven@student.hu.nl>
2021-05-08 17:15:57 +0200

    Exclude iterator formatting

--- include/Lz/Exclude.hpp
+++ include/Lz/Exclude.hpp
@@ -7,47 +7,48 @@
 #include "detail/ExcludeIterator.hpp"
 
 namespace lz {
-	template<class Iterator>
-	class Exclude final : public internal::BasicIteratorView<internal::ExcludeIterator<Iterator>> {
-	public:
-		using iterator = internal::ExcludeIterator<Iterator>;
-		using const_iterator = iterator;
+template<class Iterator>
+class Exclude final : public internal::BasicIteratorView<internal::ExcludeIterator<Iterator>> {
+public:
+	using iterator = internal::ExcludeIterator<Iterator>;
+	using const_iterator = iterator;
 
-		constexpr Exclude() = default;
+	constexpr Exclude() = default;
 
-		LZ_CONSTEXPR_CXX_17 Exclude(Iterator begin, Iterator end, const internal::DiffType<Iterator> from,
-									const internal::DiffType<Iterator> to) :
-			internal::BasicIteratorView<iterator>(iterator(begin, begin, end, from, to), iterator(end, begin, end, from, to)) {}
-	};
+	LZ_CONSTEXPR_CXX_17 Exclude(Iterator begin, Iterator end, const internal::DiffType<Iterator> from,
+								const internal::DiffType<Iterator> to) :
+		internal::BasicIteratorView<iterator>(iterator(begin, begin, end, from, to), iterator(end, begin, end, from, to)) {}
+};
 
 
-	/**
-	 * Excludes a range within [begin, end) where index is [from, to).
-	 * @param begin The beginning of the sequence.
-	 * @param end The ending of the sequence.
-	 * @param from Index to start excluding elements from.
-	 * @param to Index to stop at, note that its underlying element at this index is included.
-	 * @return An Exclude iterator view object.
-	 */
-	template<LZ_CONCEPT_ITERATOR Iterator>
-	LZ_CONSTEXPR_CXX_17 Exclude<Iterator> excludeRange(Iterator begin, Iterator end, const internal::DiffType<Iterator> from,
-													   const internal::DiffType<Iterator> to) {
-		LZ_ASSERT(std::distance(begin, end) - to >= 0, "cannot access elements after end or before begin");
-		return Exclude<Iterator>(std::move(begin), std::move(end), from, to);
-	}
+/**
+ * Excludes a range within [begin, end) where index is [from, to).
+ * @param begin The beginning of the sequence.
+ * @param end The ending of the sequence.
+ * @param from Index to start excluding elements from.
+ * @param to Index to stop at, note that its underlying element at this index is included.
+ * @return An Exclude iterator view object.
+ */
+template<LZ_CONCEPT_ITERATOR Iterator>
+LZ_CONSTEXPR_CXX_17 Exclude<Iterator> excludeRange(Iterator begin, Iterator end, const internal::DiffType<Iterator> from,
+												   const internal::DiffType<Iterator> to) {
+	using lz::distance; using std::distance;
+	LZ_ASSERT(distance(begin, end) - to >= 0, "cannot access elements after end or before begin");
+	return Exclude<Iterator>(std::move(begin), std::move(end), from, to);
+}
 
-	/**
-	 * Excludes a range within `iterable` where index is [from, to).
-	 * @param iterable The iterable sequence.
-	 * @param from Index to start excluding elements from.
-	 * @param to Index to stop at, note that its underlying element at this index is included.
-	 * @return An Exclude iterator view object.
-	 */
-	template<LZ_CONCEPT_ITERABLE Iterable, class Iterator = internal::IterTypeFromIterable<Iterable>>
-	LZ_CONSTEXPR_CXX_17 Exclude<Iterator> exclude(Iterable&& iterable, const internal::DiffType<Iterator> from,
-												  const internal::DiffType<Iterator> to) {
-		return excludeRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)), from, to);
-	}
+/**
+ * Excludes a range within `iterable` where index is [from, to).
+ * @param iterable The iterable sequence.
+ * @param from Index to start excluding elements from.
+ * @param to Index to stop at, note that its underlying element at this index is included.
+ * @return An Exclude iterator view object.
+ */
+template<LZ_CONCEPT_ITERABLE Iterable, class Iterator = internal::IterTypeFromIterable<Iterable>>
+LZ_CONSTEXPR_CXX_17 Exclude<Iterator> exclude(Iterable&& iterable, const internal::DiffType<Iterator> from,
+											  const internal::DiffType<Iterator> to) {
+	return excludeRange(internal::begin(std::forward<Iterable>(iterable)), internal::end(std::forward<Iterable>(iterable)), from, to);
+}
 }
 
 
--- include/Lz/detail/ExcludeIterator.hpp
+++ include/Lz/detail/ExcludeIterator.hpp
@@ -5,72 +5,74 @@
 
 
 namespace lz { namespace internal {
-	template<class Iterator>
-	class ExcludeIterator {
-		using IterTraits = std::iterator_traits<Iterator>;
+template<class Iterator>
+class ExcludeIterator {
+	using IterTraits = std::iterator_traits<Iterator>;
 
-	public:
-		using iterator_category = LowestIterTypeT<std::forward_iterator_tag, typename IterTraits::iterator_category>;
-		using value_type = typename IterTraits::value_type;
-		using difference_type = typename IterTraits::difference_type;
-		using reference = typename IterTraits::reference;
-		using pointer = typename IterTraits::pointer;
+public:
+	using iterator_category = typename std::common_type<std::forward_iterator_tag, typename IterTraits::iterator_category>;
+	using value_type = typename IterTraits::value_type;
+	using difference_type = typename IterTraits::difference_type;
+	using reference = typename IterTraits::reference;
+	using pointer = typename IterTraits::pointer;
 
-	private:
-		Iterator _iterator{};
-		Iterator _begin{};
-		Iterator _end{};
-		difference_type _index{};
-		difference_type _from{};
-		difference_type _to{};
+private:
+	Iterator _iterator{};
+	Iterator _begin{};
+	Iterator _end{};
+	difference_type _index{};
+	difference_type _from{};
+	difference_type _to{};
 
-	public:
-		LZ_CONSTEXPR_CXX_17 ExcludeIterator(Iterator it, Iterator begin, Iterator end, const difference_type from,
-											const difference_type to) :
-			_iterator(std::move(it)),
-			_begin(std::move(begin)),
-			_end(std::move(end)),
-			_from(from),
-			_to(to) {
-			if (it == begin && from == 0) {
-				_iterator = std::next(std::move(_iterator), _to);
-				_index = _to;
-			}
+public:
+	LZ_CONSTEXPR_CXX_17 ExcludeIterator(Iterator it, Iterator begin, Iterator end, const difference_type from,
+										const difference_type to) :
+		_iterator(std::move(it)),
+		_begin(std::move(begin)),
+		_end(std::move(end)),
+		_from(from),
+		_to(to) {
+		using std::next; using lz::next;
+		if (it == begin && from == 0) {
+			_iterator = next(std::move(_iterator), _to);
+			_index = _to;
 		}
+	}
 
-		constexpr ExcludeIterator() = default;
+	constexpr ExcludeIterator() = default;
 
-		constexpr reference operator*() const {
-			return *_iterator;
+	constexpr reference operator*() const {
+		return *_iterator;
+	}
+
+	constexpr pointer operator->() const {
+		return &*_iterator;
+	}
+
+	LZ_CONSTEXPR_CXX_17 ExcludeIterator& operator++() {
+		using std::next; using lz::next;
+		++_iterator;
+		++_index;
+		if (_index == _from) {
+			_iterator = next(std::move(_iterator), _to - _from);
 		}
+		return *this;
+	}
 
-		constexpr pointer operator->() const {
-			return &*_iterator;
-		}
+	LZ_CONSTEXPR_CXX_17 ExcludeIterator operator++(int) {
+		ExcludeIterator tmp(*this);
+		++*this;
+		return tmp;
+	}
 
-		LZ_CONSTEXPR_CXX_17 ExcludeIterator& operator++() {
-			++_iterator;
-			++_index;
-			if (_index == _from) {
-				_iterator = std::next(std::move(_iterator), _to - _from);
-			}
-			return *this;
-		}
+	constexpr friend bool operator==(const ExcludeIterator& a, const ExcludeIterator& b) {
+		return !(a != b); // NOLINT
+	}
 
-		LZ_CONSTEXPR_CXX_17 ExcludeIterator operator++(int) {
-			ExcludeIterator tmp(*this);
-			++*this;
-			return tmp;
-		}
-
-		constexpr friend bool operator==(const ExcludeIterator& a, const ExcludeIterator& b) {
-			return !(a != b); // NOLINT
-		}
-
-		constexpr friend bool operator!=(const ExcludeIterator& a, const ExcludeIterator& b) {
-			return a._iterator != b._iterator;
-		}
-	};
+	constexpr friend bool operator!=(const ExcludeIterator& a, const ExcludeIterator& b) {
+		return a._iterator != b._iterator;
+	}
+};
 }}
 
 #endif // LZ_EXCLUDE_ITERATOR_HPP
