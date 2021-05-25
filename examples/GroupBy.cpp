b56dc50fb333759baef17a42b12a46bd1ca0a79a
Marc <marc.dirven@student.hu.nl>
2021-05-25 17:40:58 +0200

    Group by example update

--- examples/GroupBy.cpp
+++ examples/GroupBy.cpp
@@ -9,20 +9,20 @@ int main() {
 	auto grouper = lz::groupBy(vec, [](const std::string& a, const std::string& b) { return a.length() == b.length(); });
 
 	for (auto&& pair : grouper) {
-		fmt::print("String length group: {}\n", pair.first);
+		fmt::print("String length group: {}\n", pair.first.length());
 		for (const auto& str : pair.second) {
-			fmt::print("{} belongs in group {}\n", pair.first, str);
+			fmt::print("value: {}\n", str);
 		}
 	}
 
 	/* Output:
-	 * String length group: 3
-	 * 3 belongs in group i'm
-	 * String length group: 4
-	 * 4 belongs in group done
-	 * String length group: 5
-	 * 5 belongs in group hello
-	 * 5 belongs in group hellp
+	String length group: 3
+	value: i'm
+	String length group: 4
+	value: done
+	String length group: 5
+	value: hello
+	value: hellp
 	 * */
 
 	fmt::print("\n");
@@ -30,17 +30,18 @@ int main() {
 	std::sort(vec.begin(), vec.end(), [](const std::string& a, const std::string& b) { return a.length() > b.length(); });
 	auto grouperDesc = lz::groupBy(vec, [](const std::string& a, const std::string& b) { return a.length() == b.length(); });
 	for (auto&& pair : grouperDesc) {
-		fmt::print("String length group: {}\n", pair.first);
+		fmt::print("String length group: {}\n", pair.first.length());
 		for (const auto& str : pair.second) {
-			fmt::print("{} belongs in group {}\n", pair.first, str);
+			fmt::print("value: {}\n", str);
 		}
 	}
-	/*String length group: 5
-	 * 5 belongs in group hello
-	 * 5 belongs in group hellp
-	 * String length group: 4
-	 * 4 belongs in group done
-	 * String length group: 3
-	 * 3 belongs in group i'm
+	/* Output:
+	String length group: 5
+	value: hello
+	value: hellp
+	String length group: 4
+	value: done
+	String length group: 3
+	value: i'm
 	 * */
 }
