81f69fb227c69a48ef57d697323733e5adf72630
Marc <marc.dirven@student.hu.nl>
2021-05-26 00:21:23 +0200

    Stringsplitter example update

--- examples/StringSplitter.cpp
+++ examples/StringSplitter.cpp
@@ -12,11 +12,12 @@ std::ostream& operator<<(std::ostream& s, fmt::string_view v) {
 int main() {
     std::string toSplit = "Hello world ";
     std::string delim = " ";
+    // Alternative: lz::split(toSplit, ' ') (using a char is faster, so use it whenever possible)
     const auto splitter = lz::split(toSplit, std::move(delim));
     std::cout << splitter << '\n';
     // Output: Hello world
-    // 
     // If C++ 17 or higher, use for (std::string_view s : lz::split) else use for (fmt::string_view substring : lz::split)
+	// If LZ_STANDALONE is defined and C++ version is less than 17, use std::string
     for (fmt::string_view substring : splitter) {
         std::cout << substring << '\n';
     }
