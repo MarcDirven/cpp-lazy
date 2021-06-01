8d7029ee149903ac2a7e6a3cce5490789b37111a
Marc <marc.dirven@student.hu.nl>
2021-06-01 22:56:19 +0200

    Added lines/unlines tests for standalone

--- tests/standalone.cpp
+++ tests/standalone.cpp
@@ -3,8 +3,8 @@
 #include <Lz/Join.hpp>
 #include <Lz/Map.hpp>
 #include <Lz/StringSplitter.hpp>
+#include <Lz/FunctionTools.hpp>
 #include <catch2/catch.hpp>
-
 
 TEST_CASE("Overall tests with LZ_STANDALONE defined") {
 	std::array<int, 4> array = {1, 2, 3, 4};
@@ -14,15 +14,23 @@ TEST_CASE("Overall tests with LZ_STANDALONE defined") {
 	REQUIRE(lz::map(array, [](int i) { return i + 1; }).toString(" ") == "2 3 4 5");
 
 	std::array<bool, 4> bools = {true, false, true, false};
-	auto boolMap = 	lz::map(bools, [](const bool b) -> std::string { return b ? "true" : "false"; }).toString(", ");
+	auto boolMap = lz::map(bools, [](const bool b) -> std::string { return b ? "true" : "false"; }).toString(", ");
 	REQUIRE(boolMap == "true, false, true, false");
 
 	std::string toSplit = "hello, world";
 	auto splitter = lz::split(toSplit, ", ");
 	REQUIRE(std::distance(splitter.begin(), splitter.end()) == 2);
 #ifdef LZ_HAS_CXX_17
-	REQUIRE(std::is_same<decltype(*splitter.begin()), std::string_view>::value);
+	REQUIRE(std::is_same_v<decltype(*splitter.begin()), std::string_view>);
 #else
 	REQUIRE(std::is_same<decltype(*splitter.begin()), std::string>::value);
 #endif
+
+	std::string lines = "hello\nworld\n";
+	auto unlined = lz::lines(lines);
+	REQUIRE(lz::lines(lines).toVector() == std::vector<std::string>{"hello", "world"});
+
+	std::vector<std::string> toLines = {"hello", "world"};
+	auto lined = lz::unlines(toLines);
+	REQUIRE(lz::unlines(toLines).toString() == std::string("hello\nworld"));
 }
