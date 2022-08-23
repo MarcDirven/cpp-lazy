d941b78c475d8601289d47f93e19cdfe6f8a0b00
Marc <marcdirven1996@outlook.com>
2022-08-23 22:37:20 +0200

    Zip improvements

--- include/Lz/Zip.hpp
+++ include/Lz/Zip.hpp
@@ -7,47 +7,6 @@
 #    include "detail/ZipIterator.hpp"
 
 namespace lz {
-namespace internal {
-template<class DiffTy, class Tuple, size_t... Is>
-LZ_CONSTEXPR_CXX_20 void doTrimTuple(const Tuple& begin, Tuple& end, const DiffTy* lengths, IndexSequence<Is...>) {
-    using lz::next;
-    using std::next;
-    const auto smallest = static_cast<DiffTy>(*std::min_element(lengths, lengths + sizeof...(Is)));
-    const int expander[] = { (std::get<Is>(end) = next(std::get<Is>(begin), smallest), 0)... };
-    static_cast<void>(expander);
-}
-
-template<class... Iterators, class DiffTy = typename std::common_type<DiffType<Iterators>...>::type>
-LZ_CONSTEXPR_CXX_20 void trimTuple(const std::tuple<Iterators...>& begin, std::tuple<Iterators...>& end, const DiffTy* lengths) {
-    constexpr size_t argLength = sizeof...(Iterators);
-    static_assert(argLength > 1, "zip requires more than 1 container/iterable");
-    doTrimTuple(begin, end, lengths, MakeIndexSequence<argLength>());
-}
-
-template<class Tuple, class DiffTy, std::size_t... Is>
-LZ_CONSTEXPR_CXX_20 void doGetLengthFromIterators(const Tuple& begin, const Tuple& end, DiffTy* lengths, IndexSequence<Is...>) {
-    auto expander = { (lengths[Is] = getIterLength(std::get<Is>(begin), std::get<Is>(end)), 0)... };
-    static_cast<void>(expander);
-}
-
-template<class DiffTy, class... Iterators>
-LZ_CONSTEXPR_CXX_20 void
-getLengthFromIterators(const std::tuple<Iterators...>& begin, const std::tuple<Iterators...>& end, DiffTy* lengths) {
-    doGetLengthFromIterators(begin, end, lengths, MakeIndexSequence<sizeof...(Iterators)>());
-}
-
-template<class DiffTy, class... Iterables, std::size_t... Is>
-LZ_CONSTEXPR_CXX_20 void doGetLengthFromIterables(DiffTy* out, IndexSequence<Is...>, const Iterables&... iterables) {
-    auto expander = { (out[Is] = static_cast<DiffTy>(iterables.size()), 0)... };
-    static_cast<void>(expander);
-}
-
-template<class DiffTy, class... Iterables>
-LZ_CONSTEXPR_CXX_20 void getLengthFromIterables(DiffTy* out, const Iterables&... iterables) {
-    doGetLengthFromIterables(out, internal::MakeIndexSequence<sizeof...(Iterables)>(), iterables...);
-}
-} // namespace internal
-
 template<LZ_CONCEPT_ITERATOR... Iterators>
 class Zip final : public internal::BasicIteratorView<internal::ZipIterator<Iterators...>> {
 public:
@@ -71,7 +30,7 @@ public:
 
 /**
  * @brief This function can be used to iterate over multiple containers. It stops at its smallest container.
- * Its `begin()` function returns a random access iterator. The operators `<, <=, >, >=` will return true
+ * Its `begin()` function returns an iterator. The operators `<, <=, >, >=` will return true
  * if one of the containers returns true with its corresponding `operator<`/`operator<=`/`operator>`/`operator>=`.
  * @details The tuple that is returned by `operator*` returns a `std::tuple` by value and its elements by
  * reference e.g. `std::tuple<Args&...>`. So it is possible to alter the values in the container/iterable),
@@ -83,16 +42,12 @@ public:
  */
 template<LZ_CONCEPT_ITERATOR... Iterators>
 LZ_NODISCARD LZ_CONSTEXPR_CXX_20 Zip<Iterators...> zipRange(std::tuple<Iterators...> begin, std::tuple<Iterators...> end) {
-    using DiffTy = typename std::common_type<internal::DiffType<Iterators>...>::type;
-    DiffTy lengths[sizeof...(Iterators)]{};
-    internal::getLengthFromIterators(begin, end, lengths);
-    internal::trimTuple(begin, end, lengths);
     return { std::move(begin), std::move(end) };
 }
 
 /**
  * @brief This function can be used to iterate over multiple containers. It stops at its smallest container.
- * Its `begin()` function returns a random access iterator. The operators `<, <=, >, >=` will return true
+ * Its `begin()` function returns an iterator. The operators `<, <=, >, >=` will return true
  * if one of the containers returns true with its corresponding `operator<`/`operator<=`/`operator>`/`operator>=`.
  * @details The tuple that is returned by `operator*` returns a `std::tuple` by value and its elements by
  * reference e.g. `std::tuple<Args&...>`. So it is possible to alter the values in the container/iterable),
@@ -103,13 +58,9 @@ LZ_NODISCARD LZ_CONSTEXPR_CXX_20 Zip<Iterators...> zipRange(std::tuple<Iterators
  */
 template<LZ_CONCEPT_ITERABLE... Iterables>
 LZ_NODISCARD LZ_CONSTEXPR_CXX_20 Zip<internal::IterTypeFromIterable<Iterables>...> zip(Iterables&&... iterables) {
-    using DiffTy = typename std::common_type<internal::DiffTypeIterable<Iterables>...>::type;
-    DiffTy lengths[sizeof...(Iterables)]{};
-    internal::getLengthFromIterables(lengths, iterables...);
     auto begin = std::make_tuple(internal::begin(std::forward<Iterables>(iterables))...);
     auto end = std::make_tuple(internal::end(std::forward<Iterables>(iterables))...);
-    internal::trimTuple(begin, end, lengths);
-    return { std::move(begin), std::move(end) };
+    return lz::zipRange(std::move(begin), std::move(end));
 }
 
 // End of group
