903c60f4e52274d443ce9b082b83bf2afec7c3d6
Marc <marcdirven1996@outlook.com>
2022-09-06 22:18:57 +0200

    Added output

--- examples/Chunks.cpp
+++ examples/Chunks.cpp
@@ -7,7 +7,14 @@ int main() {
 	for (auto&& chunk : chunks) {
 		fmt::print("This chunk has length {}\n", std::distance(chunk.begin(), chunk.end()));
 		for (const int i : chunk) {
-			fmt::print("{}\n", i);
+			fmt::print("{} ", i);
 		}
 	}
+	// Output
+	// This chunk has length 3
+	// 1 2 3 
+	// This chunk has length 3
+	// 4 5 6
+	// This chunk has length 2
+	// 7 8
 }
