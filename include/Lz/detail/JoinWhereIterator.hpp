455b60bc56ee57db2bedc3e8d231d9b4633c5f1c
Marc <marcdirven1996@outlook.com>
2022-09-08 22:34:32 +0200

    Added mutexc

--- include/Lz/detail/JoinWhereIterator.hpp
+++ include/Lz/detail/JoinWhereIterator.hpp
@@ -39,7 +39,7 @@ private:
     void findNext() {
 #ifdef LZ_HAS_EXECUTION
         if constexpr (checkForwardAndPolicies<Execution, IterA>()) {
-            _iterA = std::find_if(_iterA, _endA, [this, &mutex](const ValueType<IterA>& a) {
+            _iterA = std::find_if(_iterA, _endA, [this](const ValueType<IterA>& a) {
                 auto&& toFind = _selectorA(a);
                 _iterB =
                     std::lower_bound(std::move(_iterB), _endB, toFind,
@@ -54,7 +54,7 @@ private:
         else {
             std::mutex mutex;
             std::lock_guard guard(mutex);
-            _iterA = std::find_if(_exec, _iterA, _endA, [this](const ValueType<IterA>& a) {
+            _iterA = std::find_if(_exec, _iterA, _endA, [this, &mutex](const ValueType<IterA>& a) {
                 auto&& toFind = _selectorA(a);
                 _iterB =
                     std::lower_bound(std::move(_iterB), _endB, toFind,
