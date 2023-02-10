2a21124e953a070da3132c63676e03471d58fd26
Marc <marcdirven1996@outlook.com>
2023-02-10 16:51:09 +0100

    Added more lua support

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,17 @@ option("tests", {default = false, description = "Enable tests"})
 
 add_requires("fmt >=5")
 
-target("cpp-lazy")
-    set_kind("headeronly")
-    add_includedirs("include", { interface = true })
-    add_packages("fmt", { public = true })
-
 if has_config("tests") then
     add_requires("catch2 v2.13.10")
+end
 
+target("cpp-lazy")
+    set_kind("headeronly")
+    add_includedirs("include", { public = true })
+    add_packages("fmt", { public = true })
+target_end()
+
+if has_config("tests") then
     target("cpp-lazy tests")
         set_kind("binary")
         add_files("tests/*.cpp")
