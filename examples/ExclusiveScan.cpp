f4e1774264f10f63631cc9ee27f4b9f4eacbfa22
Marc <marcdirven1996@outlook.com>
2023-03-10 10:46:59 +0100

    Added exclusive scan examples

---
+++ examples/ExclusiveScan.cpp
@@ -0,0 +1,23 @@
+#include <Lz/ExclusiveScan.hpp>
+
+#include <iostream>
+
+int main() {
+    int array[] = {3, 5, 2, 3, 4, 2, 3};
+    // start the scan from 0
+    auto scan = lz::eScan(array, 0);
+
+    for (const int& i : scan) {
+        fmt::print("{} ", i);
+    }
+    // prints 0 3 8 10 13 17 19
+
+    // essentially it's:
+    // 0 (0)
+    // 0 + 3 (3)
+    // 3 + 5 (8)
+    // 8 + 2 (10)
+    // 10 + 3 (13)
+    // 13 + 4 (17)
+    // 17 + 2 (19)
+}
