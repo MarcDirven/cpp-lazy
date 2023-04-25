5ee8d81ba6ccaa808a97c0ac1c1eb9431d38a149
Marc <31624073+MarcDirven@users.noreply.github.com>
2023-04-25 11:25:51 +0200

    Update Generate.cpp

--- examples/Generate.cpp
+++ examples/Generate.cpp
@@ -4,13 +4,16 @@
 #include <iostream>
 
 int main() {
-    int myIncreasingCounter = 0;
-    constexpr int amount = 4;
-    auto generator = [&myIncreasingCounter]() {
-        return myIncreasingCounter++;
-    };
-    const auto gen = lz::generate(generator, amount);
-    std::cout << gen << '\n';
+    	std::size_t counter = 0;
+	static constexpr std::size_t amount = 4;
+    	auto gen = lz::generate(
+        	[](std::size_t& c) {
+            		auto tmp{ c++ };
+            		return tmp;
+		},
+        	amount, counter);
+
+    	std::cout << gen << '\n';
 	// Output: 0 1 2 3
 	for (int i : gen) {
 		// Process i...
