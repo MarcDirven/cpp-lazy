9e60453b17d374618520c6dfab426d87b087e0c2
MarcDirven <github@mdy.mozmail.com>
2024-09-06 21:25:26 +0200

    Formatting

--- examples/Take.cpp
+++ examples/Take.cpp
@@ -7,7 +7,7 @@ int main() {
     std::vector<int> seq = { 1, 2, 3, 4, 5, 6 };
 
     auto taken = lz::take(seq, 3);
-fmt::print("{}\n", taken); // output: 1, 2, 3
+    fmt::print("{}\n", taken); // output: 1, 2, 3
 
     auto dropped = lz::drop(seq, 3);
     fmt::print("{}\n", dropped); // output: 4, 5, 6
