b14c2db5d6f91864017a9f8d7ba1a7b7f0697127
MarcDirven <github@mdy.mozmail.com>
2024-12-12 10:23:50 +0100

    added sentinel tag for generateiterator

--- include/Lz/detail/iterators/GenerateIterator.hpp
+++ include/Lz/detail/iterators/GenerateIterator.hpp
@@ -11,13 +11,14 @@
 namespace lz {
 namespace detail {
 template<class GeneratorFunc, class... Args>
-class GenerateIterator
-    : public IterBase<GenerateIterator<GeneratorFunc, Args...>, TupleInvokerType<GeneratorFunc, Args...>,
-                      FakePointerProxy<TupleInvokerType<GeneratorFunc, Args...>>, std::ptrdiff_t, std::forward_iterator_tag> {
+class GenerateIterator : public IterBase<GenerateIterator<GeneratorFunc, Args...>, TupleInvokerType<GeneratorFunc, Args...>,
+                                         FakePointerProxy<TupleInvokerType<GeneratorFunc, Args...>>, std::ptrdiff_t,
+                                         std::forward_iterator_tag, DefaultSentinel> {
 
-    std::size_t _current{};
-    mutable TupleInvoker<GeneratorFunc, Args...> _tupleInvoker{};
     mutable std::tuple<Args...> _args{};
+    mutable TupleInvoker<GeneratorFunc, Args...> _tupleInvoker{};
+    std::size_t _current{};
+    std::size_t _amount{};
     bool _isWhileTrueLoop{ false };
 
 public:
@@ -30,10 +31,11 @@ public:
     constexpr GenerateIterator() = default;
 
     LZ_CONSTEXPR_CXX_14
-    GenerateIterator(const std::size_t start, GeneratorFunc generatorFunc, const bool isWhileTrueLoop, std::tuple<Args...> args) :
-        _current(start),
-        _tupleInvoker(makeExpandFn(std::move(generatorFunc), MakeIndexSequence<sizeof...(Args)>())),
+    GenerateIterator(const std::size_t amount, GeneratorFunc generatorFunc, const bool isWhileTrueLoop,
+                     std::tuple<Args...> args) :
         _args(std::move(args)),
+        _tupleInvoker(makeExpandFn(std::move(generatorFunc), MakeIndexSequence<sizeof...(Args)>())),
+        _amount(amount),
         _isWhileTrueLoop(isWhileTrueLoop) {
     }
 
@@ -54,6 +56,10 @@ public:
     LZ_NODISCARD constexpr bool eq(const GenerateIterator& b) const noexcept {
         LZ_ASSERT(_isWhileTrueLoop == b._isWhileTrueLoop, "incompatible iterator types: both must be while true or not");
         return _current == b._current;
+    }
+
+    LZ_NODISCARD constexpr bool eq(DefaultSentinel) const noexcept {
+        return _current == _amount;
     }
 };
 } // namespace detail
